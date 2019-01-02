01a5cd5442b5538e1f4335dc8f93da4a39dfbcc4
Adam Theisen <atheisen@anl.gov>
2019-01-02 15:38:27 -0600

    Removing the old NaN insertion code

--- act/utils/data_utils.py
+++ act/utils/data_utils.py
@@ -13,7 +13,10 @@ def add_in_nan(time,data):
         n_obs = np.floor((time[i+1]-time[i])/mode/np.timedelta64(1,'s'))
         time_arr = [d_time[i+offset]+np.timedelta64(int((n+1)*mode),'s') for n in range(int(n_obs))]
         S = d_data.shape
-        data_arr = np.empty([len(time_arr),S[1]])
+        if len(S) == 2:
+            data_arr = np.empty([len(time_arr),S[1]])
+        else:
+            data_arr = np.empty([len(time_arr)])
         data_arr[:] = np.nan
 
         d_time = np.insert(d_time,i+1+offset,time_arr)
