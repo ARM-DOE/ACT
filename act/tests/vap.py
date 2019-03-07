d9b98a10fcea1b8fabb58b5985ce0a86964ae629
Adam Theisen <atheisen@anl.gov>
2019-03-07 09:08:42 -0600

    Updates to output netcdf file

--- act/tests/vap.py
+++ act/tests/vap.py
@@ -2,16 +2,12 @@ import matplotlib
 matplotlib.use('Agg')
 
 import act.io.armfiles as arm
-from act.corrections.ceil import correct_ceil
-from act.plotting import common
 import act.plotting.plot as armplot
-import act.utils.data_utils as arm_du
 import act.discovery.get_files as get_data
 
 import glob
 import matplotlib.pyplot as plt
-import numpy as np
-import sys
+import os
 
 username='' #Add your credentials from the ARM live data web service
 token='' # Accessed through the Data Discovery homepage
@@ -19,7 +15,7 @@ datastream = 'sgpmetE13.b1'
 startdate = '2019-01-01'
 enddate = '2019-01-07'
 
-get_data.download_data(username, token, datastream, startdate, enddate)
+#get_data.download_data(username, token, datastream, startdate, enddate)
 
 files = glob.glob(''.join(['./',datastream,'/*']))
 met = arm.read_netcdf(files)
@@ -30,13 +26,16 @@ met['met_lcl'] = met_lcl*1000.
 met['met_lcl'].attrs['units'] = 'm'
 met['met_lcl'].attrs['long_name'] = 'LCL Calculated from SGP MET E13'
 
+cwd = os.getcwd()
+met['met_lcl'].to_netcdf(path=cwd+'/met_lcl.nc',mode='w',engine='netcdf4')
+
 display = armplot.display(met)
 fig = plt.figure(figsize=(10,6))
 ax = plt.subplot(1,1,1)
 display.plot('met_lcl',ax=ax)
 
 datastream = 'sgpceilC1.b1'
-get_data.download_data(username, token, datastream, startdate, enddate)
+#get_data.download_data(username, token, datastream, startdate, enddate)
 files = glob.glob(''.join(['./',datastream,'/*']))
 ceil = arm.read_netcdf(files)
 display = armplot.display(ceil)
