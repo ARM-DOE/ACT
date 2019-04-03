2904f9dc437f16844658c274b3f304fa00196693
Kenneth Kehoe <kkehoe@ou.edu>
2019-04-03 12:41:03 -0600

    Added a test for clean and updated the sample files to correctly use MET instead of SONDE

--- act/tests/sample_files.py
+++ act/tests/sample_files.py
@@ -15,6 +15,10 @@ import os
 
 DATA_PATH = os.path.join(os.path.dirname(__file__), 'data')
 
-EXAMPLE_SONDE1 = os.path.join(DATA_PATH, 'sgpmetE13.b1.20190101.000000.cdf')
+EXAMPLE_MET1 = os.path.join(DATA_PATH, 'sgpmetE13.b1.20190101.000000.cdf')
+EXAMPLE_CEIL1 = os.path.join(DATA_PATH, 'sgpceilC1.b1.20190101.000000.nc')
+EXAMPLE_SONDE1 = os.path.join(DATA_PATH, 'sgpsondewnpnC1.b1.20190101.053200.cdf')
 EXAMPLE_LCL1 = os.path.join(DATA_PATH, 'met_lcl.nc')
-EXAMPLE_SONDE_WILDCARD = os.path.join(DATA_PATH, 'sgpmet*.cdf')
+EXAMPLE_SONDE_WILDCARD = os.path.join(DATA_PATH, 'sgpsondewnpn*.cdf')
+EXAMPLE_MET_WILDCARD = os.path.join(DATA_PATH, 'sgpmet*.cdf')
+EXAMPLE_CEIL_WILDCARD = os.path.join(DATA_PATH, 'sgpceil*.cdf')
--- act/tests/test_io.py
+++ act/tests/test_io.py
@@ -1,10 +1,8 @@
 import act
-import glob
-
 
 def test_io():
     sonde_ds = act.io.armfiles.read_netcdf(
-        [act.tests.EXAMPLE_SONDE1])
+        [act.tests.EXAMPLE_MET1])
     assert 'temp_mean' in sonde_ds.variables.keys()
     assert 'rh_mean' in sonde_ds.variables.keys()
     assert sonde_ds.act.arm_standards_flag.OK
@@ -13,7 +11,7 @@ def test_io():
 
 def test_io_mfdataset():
     sonde_ds = act.io.armfiles.read_netcdf(
-        act.tests.EXAMPLE_SONDE_WILDCARD)
+        act.tests.EXAMPLE_MET_WILDCARD)
     assert 'temp_mean' in sonde_ds.variables.keys()
     assert 'rh_mean' in sonde_ds.variables.keys()
     assert len(sonde_ds.act.file_times) == 7
