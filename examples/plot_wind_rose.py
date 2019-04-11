5bd30aa7ae499331a2676f8343f451bdf82fbf01
rcjackson <rjackson@anl.gov>
2019-04-11 16:35:42 -0500

    DOC: Now can access class methods + more PEP8

--- examples/plot_wind_rose.py
+++ examples/plot_wind_rose.py
@@ -16,14 +16,14 @@ from matplotlib import pyplot as plt
 sonde_ds = act.io.armfiles.read_netcdf(
     act.tests.sample_files.EXAMPLE_TWP_SONDE_WILDCARD)
 
-WindDisplay = act.plotting.WindRoseDisplay(sonde_ds, figsize=(8,10))
+WindDisplay = act.plotting.WindRoseDisplay(sonde_ds, figsize=(8, 10))
 WindDisplay.plot('deg', 'wspd',
                  spd_bins=np.linspace(0, 25, 5), num_dirs=30,
                  tick_interval=2)
 plt.show()
 
 BarbDisplay = act.plotting.TimeSeriesDisplay(
-    {'sonde_darwin': sonde_ds}, figsize=(10,5))
+    {'sonde_darwin': sonde_ds}, figsize=(10, 5))
 BarbDisplay.plot_time_height_xsection_from_1d_data('rh', 'pres',
                                                    cmap='coolwarm_r',
                                                    vmin=0, vmax=100,
@@ -31,5 +31,3 @@ BarbDisplay.plot_time_height_xsection_from_1d_data('rh', 'pres',
 BarbDisplay.plot_barbs_from_spd_dir('deg', 'wspd', 'pres',
                                     num_barbs_x=20)
 plt.show()
-
-
