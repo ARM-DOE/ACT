3736fa30e0195e553eb9e5273c2f0f44dbaddd90
Adam Theisen <atheisen@anl.gov>
2019-04-16 09:08:05 -0500

    FIX: Fixing a bug with the unit test

--- act/tests/test_io.py
+++ act/tests/test_io.py
@@ -20,9 +20,17 @@ def test_io_mfdataset():
     sonde_ds.close()
 
 def test_io_anl_csv():
+    headers = ['day','month','year','time','pasquill',
+               'wdir_60m','wspd_60m','wdir_60m_std',
+               'temp_60m','wdir_10m','wspd_10m',
+               'wdir_10m_std','temp_10m','temp_dp',
+               'rh','avg_temp_diff','total_precip',
+               'solar_rad','net_rad','atmos_press',
+               'wv_pressure','temp_soil_10cm',
+               'temp_soil_100cm','temp_soil_10ft']
     anl_ds = act.io.csvfiles.read_csv(
-        act.tests.EXAMPLE_ANL_CSV)
+        act.tests.EXAMPLE_ANL_CSV, sep='\s+', column_names=headers)
     assert 'temp_60m' in anl_ds.variables.keys()
     assert 'rh' in anl_ds.variables.keys()
-    assert anl_ds.act.arm_standards_flag.OK
+    assert obj['temp_60m'].values[10] == -1.7
     anl_ds.close()
