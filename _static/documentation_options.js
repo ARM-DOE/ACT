41300128b427fa3e262c596651284337ba765d7c
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2019-04-23 21:59:27 +0000

    Update docs after building Travis build 93 of ANL-DIGR/ACT
    The docs were built from the branch 'master' against the commit
    f95453a2173180654f31ec67ab49c515dd067fbf.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/ANL-DIGR/ACT/jobs/523702567.
    
    The doctr command that was run is
    
        /home/travis/miniconda3/envs/testenv/bin/doctr deploy .

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.post142+g027a0b5',
+    VERSION: '0.post147+gf95453a',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
