869ac7ceee70cc10c675597b7f38837624dda7ce
Adam Theisen <atheisen@anl.gov>
2019-05-09 14:19:52 -0500

    ENH: Full overlap and afterpulse corrections implemented

--- act/corrections/mpl.py
+++ act/corrections/mpl.py
@@ -15,6 +15,8 @@ def correct_mpl(obj):
     4.) Range Correction
     5.) Overlap Correction (Multiply)
 
+    Note: Deadtime and darkcount corrections are not being applied yet
+
     Parameters
     ----------
     obj: Dataset object
@@ -44,7 +46,8 @@ def correct_mpl(obj):
         signal_bg = signal.mean(dim = 'dim_0').values
   
         # Seems to be the fastest way of removing background signal at the moment
-        data = obj[name].values
+        data = obj[name].where(obj[name] >0.).values
+        
         for i in range(len(obj['time'].values)):
             data[i,:] = data[i,:]-signal_bg[i]
 
@@ -53,10 +56,10 @@ def correct_mpl(obj):
         ap = obj['afterpulse_correction_'+mode].values
 
         # Overlap Correction Variable
-        op = obj['overlap_correction'].values
+        op = obj['overlap_correction'].values[0,:,0]
+        op_height = obj['overlap_correction_heights'].values[0,:,0]
 
-        print(ap,np.shape(ap))
-        for j in range(len(obj['height'].values)):
+        for j in range(len(obj['range_bins'].values)):
             # Afterpulse Correction
             data[:,j] = data[:,j] - ap[:,j]
 
@@ -64,11 +67,24 @@ def correct_mpl(obj):
             data[:,j] = data[:,j] * height[:,j] ** 2.
 
             # Overlap Correction
+            idx = (np.abs(op_height - height[0,j])).argmin()
+            data[:,j] = data[:,j] * op[idx]
+
+        #Get linear data for ratio
+        if 'co_pol' in name:
+           co_data = data
+        if 'cross_pol' in name:
+           x_data = data
+
+        # Convert data to decibels
+        data = 10.*np.log10(data)
+
+        # Write data to object
         obj[name].values = data
- 
 
-    corrections=['afterpulse_correction','overlap_correction']
-
-    
+    # Create the co/cross ratio variable
+    ratio = (x_data/co_data)*100.
+    #obj['cross_co_ratio'] = xr.DataArray(ratio)
+    obj['cross_co_ratio'] = obj[var_names[0]].copy(data=ratio)
 
     return obj
