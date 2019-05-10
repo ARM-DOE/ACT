205a80c99ce6d37283f26c3cd75840dfa89562aa
Adam Theisen <atheisen@anl.gov>
2019-05-10 13:07:58 -0500

    ENH: Final updates to improve the speed of the corrections

--- act/corrections/mpl.py
+++ act/corrections/mpl.py
@@ -29,79 +29,73 @@ def correct_mpl(obj):
         The ACT Object containing the corrected values.
     """
 
-    # 1 - Remove negative height data
-    print('Removing Heights <0')
+    # Get some variables before processing begins
     act = obj.act
-    obj = obj.where(obj.height > 0)
+    # Overlap Correction Variable
+    op = obj['overlap_correction'].values[0,:]
+    op_height = obj['overlap_correction_heights'].values[0,:]
+
+    # 1 - Remove negative height data
+    obj = obj.where(obj.height > 0,drop=True)
     height = obj['height'].values
 
+    # The drop strips out the ACT data so re-populating
+    obj.act = act
+
     # Get indices for calculating background
-    print('Background')
-    var_names = ['signal_return_co_pol','signal_return_cross_pol']
-    ind = [obj.height.shape[1]-50,obj.height.shape[1]-2]
+    var_names = ['signal_return_co_pol', 'signal_return_cross_pol']
+    ind = [obj.height.shape[1]-50, obj.height.shape[1]-2]
 
     # Subset last gates into new dataset 
-    print('subset')
-    dummy = obj.isel(range_bins=xr.DataArray(np.arange(ind[0],ind[1])))
-
-    #obj.rename({'dim_0': 'range_bins'},inplace=True)
-    obj.act = act
+    dummy = obj.isel(range_bins=xr.DataArray(np.arange(ind[0], ind[1])))
 
     #Turn off warnings
     warnings.filterwarnings("ignore")
  
     # Run through co and cross pol data for corrections
-    for name in var_names:    
-        print('BG Calc')
-        signal = dummy[name]
-        signal = signal.where(signal > -9998.)
-        signal_bg = signal.mean(dim = 'dim_0').values
+    co_bg = dummy[var_names[0]]
+    co_bg = co_bg.where(co_bg > -9998.)
+    co_bg = co_bg.mean(dim = 'dim_0').values
+
+    x_bg = dummy[var_names[1]]
+    x_bg = x_bg.where(x_bg > -9998.)
+    x_bg = x_bg.mean(dim = 'dim_0').values
   
-        # Seems to be the fastest way of removing background signal at the moment
-        data = obj[name].where(obj[name] >0.).values
-        
-        for i in range(len(obj['time'].values)):
-            data[i,:] = data[i,:]-signal_bg[i]
+    # Seems to be the fastest way of removing background signal at the moment
+    co_data = obj[var_names[0]].where(obj[var_names[0]] > 0).values
+    x_data = obj[var_names[1]].where(obj[var_names[1]] > 0).values
+    for i in range(len(obj['time'].values)):
+        co_data[i,:] = co_data[i,:] - co_bg[i]
+        x_data[i,:] = x_data[i,:] - x_bg[i]
 
-        # After Pulse Correction Variable
-        mode = '_'.join(name.split('_')[-2:])
-        ap = obj['afterpulse_correction_'+mode].values
+    # After Pulse Correction Variable
+    co_ap = obj['afterpulse_correction_co_pol'].values
+    x_ap = obj['afterpulse_correction_cross_pol'].values
 
-        # Overlap Correction Variable
-        op = obj['overlap_correction'].values[0,:,-1]
-        op_height = obj['overlap_correction_heights'].values[0,:,-1]
+    for j in range(len(obj['range_bins'].values)):
+        # Afterpulse Correction
+        co_data[:,j] = co_data[:,j] - co_ap[:,j]
+        x_data[:,j] = x_data[:,j] - x_ap[:,j]
 
-        for j in range(len(obj['range_bins'].values)):
-            print(j)
-            # Afterpulse Correction
-            data[:,j] = data[:,j] - ap[:,j]
+        # R-Squared Correction
+        co_data[:,j] = co_data[:,j] * height[:,j] ** 2.
+        x_data[:,j] = x_data[:,j] * height[:,j] ** 2.
 
-            # R-Squared Correction
-            data[:,j] = data[:,j] * height[:,j] ** 2.
+        # Overlap Correction
+        idx = (np.abs(op_height - height[0,j])).argmin()
+        co_data[:,j] = co_data[:,j] * op[idx]
+        x_data[:,j] = x_data[:,j] * op[idx]
 
-            # Overlap Correction
-            idx = (np.abs(op_height - height[0,j])).argmin()
-            data[:,j] = data[:,j] * op[idx]
+    # Convert data to decibels
+    co_data = 10. * np.log10(co_data)
+    x_data = 10. * np.log10(x_data)
 
-        #Get linear data for ratio
-        if 'co_pol' in name:
-           co_data = data
-        if 'cross_pol' in name:
-           x_data = data
-
-        # Convert data to decibels
-        print('Convert Data to Log')
-        data = 10.*np.log10(data)
-
-        # Write data to object
-        print('Write to obj')
-        obj[name].values = data
+    # Write data to object
+    obj[var_names[0]].values = co_data
+    obj[var_names[1]].values = x_data
 
     # Create the co/cross ratio variable
-    print('Ratio')
-    ratio = (x_data/co_data)*100.
-    print('Copy OBJ')
+    ratio = (x_data/co_data) * 100.
     obj['cross_co_ratio'] = obj[var_names[0]].copy(data=ratio)
 
-    print('Done')
     return obj
