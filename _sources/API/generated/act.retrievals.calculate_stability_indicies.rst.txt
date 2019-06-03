43fc06ad953ce8527bb498bfa539d7ac93b043c9
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2019-06-03 19:03:14 +0000

    Update docs after building Travis build 149 of ANL-DIGR/ACT
    The docs were built from the branch 'master' against the commit
    c7b23123c931de55c85a10bf4eb16e86885fb490.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/ANL-DIGR/ACT/jobs/540897476.
    
    The doctr command that was run is
    
        /home/travis/miniconda3/envs/testenv/bin/doctr deploy .

---
+++ _sources/API/generated/act.retrievals.calculate_stability_indicies.rst.txt
@@ -0,0 +1,6 @@
+act.retrievals.calculate\_stability\_indicies
+=============================================
+
+.. currentmodule:: act.retrievals
+
+.. autofunction:: calculate_stability_indicies
--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.post225+g5d9b7b8',
+    VERSION: '0.post229+gc7b2312',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
