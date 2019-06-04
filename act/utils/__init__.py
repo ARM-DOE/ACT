4810dd988794e43861f03ebd34b4bed483cf35ee
zssherman <zsherman@anl.gov>
2019-06-04 13:38:11 -0500

    ENH: Have imports from data_utils and datetime_utils module no longer need extension.

--- act/utils/__init__.py
+++ act/utils/__init__.py
@@ -5,17 +5,18 @@ act.utils (act.utils)
 
 .. currentmodule:: act.utils
 
-This module contains the common procedues used by all modules of the ARM
+This module contains the common procedures used by all modules of the ARM
 Community Toolkit.
 
 .. autosummary::
     :toctree: generated/
 
-    datetime_utils.dates_between
-    data_utils.add_in_nan
-    data_utils.get_missing_value
-    data_utils.assign_coordinates
+    dates_between
+    add_in_nan
+    get_missing_value
+    assign_coordinates
 """
 
-from . import datetime_utils
-from . import data_utils
+from .data_utils import add_in_nan, get_missing_value
+from .data_utils import assign_coordinates
+from .datetime_utils import dates_between
