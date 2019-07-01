662c9c9e8ab170b87136c5f8f3c88119155fef62
kehoe <kkehoe@ou.edu>
2019-07-01 22:29:08 +0000

    pep8 updates

--- act/tests/test_qc.py
+++ act/tests/test_qc.py
@@ -1,6 +1,7 @@
 from act.io.armfiles import read_netcdf
 from act.tests import EXAMPLE_IRT25m20s
 
+
 def test_qc_init():
     ds_object = read_netcdf(EXAMPLE_IRT25m20s)
     var_name = 'inst_up_long_dome_resist'
@@ -19,7 +20,3 @@ def test_qc_init():
     assert ds_object[qc_var_name].attrs['standard_name'] == 'quality_flag'
     assert ds_object[var_name].attrs['ancillary_variables'] == qc_var_name
     ds_object.close()
-
-
-
-
