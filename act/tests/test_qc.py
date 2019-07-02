334eb70ab093cd3edd9f9ee8104e9d83496ad870
kehoe <kkehoe@ou.edu>
2019-07-02 19:12:22 +0000

    Updated to work with new method and more tests

--- act/tests/test_qc.py
+++ act/tests/test_qc.py
@@ -1,5 +1,6 @@
 from act.io.armfiles import read_netcdf
 from act.tests import EXAMPLE_IRT25m20s
+import numpy as np
 
 
 def test_qc_init():
@@ -8,15 +9,36 @@ def test_qc_init():
     expected_qc_var_name = 'qc_' + var_name
 
     # Perform adding of quality control variables to object
-    qc_var_name = ds_object.qcfilter.check_for_ancillary_qc(var_name)
+    result = ds_object.qcfilter.add_test(var_name, test_meaning='Birds!')
+    assert isinstance(result, dict)
+    qc_var_name = result['qc_variable_name']
     assert qc_var_name == expected_qc_var_name
+
+    # Check that new linking and describing attributes are set
+    assert ds_object[qc_var_name].attrs['standard_name'] == 'quality_flag'
+    assert ds_object[var_name].attrs['ancillary_variables'] == qc_var_name
 
     # Check that CF attributes are set including new flag_assessments
     assert 'flag_masks' in ds_object[qc_var_name].attrs.keys()
     assert 'flag_meanings' in ds_object[qc_var_name].attrs.keys()
     assert 'flag_assessments' in ds_object[qc_var_name].attrs.keys()
 
-    # Check that new linking and describing attributes are set
-    assert ds_object[qc_var_name].attrs['standard_name'] == 'quality_flag'
-    assert ds_object[var_name].attrs['ancillary_variables'] == qc_var_name
+    # Check that the values of the attributes are set correctly
+    assert ds_object[qc_var_name].attrs['flag_assessments'][0] == 'Bad'
+    assert ds_object[qc_var_name].attrs['flag_meanings'][0] == 'Birds!'
+    assert ds_object[qc_var_name].attrs['flag_masks'][0] == 1
+
+    # Set some tests and assure they are correct
+    index = [0, 1, 2, 30]
+    ds_object.qcfilter.set_test(var_name, index=index,
+                                test_number=result['test_number'])
+    assert np.sum(ds_object.qcfilter.get_qc_test_mask(
+                      var_name, result['test_number'], return_index=True) -
+                  np.array(index, dtype=np.int)) == 0
+    # Unset a test
+    ds_object.qcfilter.unset_test(var_name, index=0,
+                                  test_number=result['test_number'])
+    # Remove the test
+    ds_object.qcfilter.remove_test(var_name,
+                                   test_number=result['test_number'])
     ds_object.close()
