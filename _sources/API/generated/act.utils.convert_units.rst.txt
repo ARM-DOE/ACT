8ba579804a0536bc5aab049f29e2e1fc46d9e08b
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2019-07-14 20:36:00 +0000

    Update docs after building Travis build 180 of ANL-DIGR/ACT
    The docs were built from the branch 'master' against the commit
    caeb37f9221ad60b23fe8d8665756a2ebbe85c69.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/ANL-DIGR/ACT/jobs/558652456.
    
    The doctr command that was run is
    
        /home/travis/miniconda3/envs/testenv/bin/doctr deploy .

---
+++ _sources/API/generated/act.utils.convert_units.rst.txt
@@ -0,0 +1,6 @@
+act.utils.convert\_units
+========================
+
+.. currentmodule:: act.utils
+
+.. autofunction:: convert_units
--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.post291+g6617b7d',
+    VERSION: '0.post294+gcaeb37f',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
