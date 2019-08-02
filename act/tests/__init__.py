18b6355ed531459c8f1e28b849b1b0e82ba9ada2
Adam Theisen <atheisen@anl.gov>
2019-08-02 12:54:26 -0500

    ADD: Adding test for contour plots

--- act/tests/__init__.py
+++ act/tests/__init__.py
@@ -20,4 +20,5 @@ from .sample_files import (EXAMPLE_SONDE1, EXAMPLE_LCL1,
                            EXAMPLE_SONDE_WILDCARD, EXAMPLE_MET1,
                            EXAMPLE_MET_WILDCARD, EXAMPLE_CEIL1,
                            EXAMPLE_CEIL_WILDCARD, EXAMPLE_ANL_CSV,
-                           EXAMPLE_MPL_1SAMPLE, EXAMPLE_IRT25m20s)
+                           EXAMPLE_MPL_1SAMPLE, EXAMPLE_IRT25m20s,
+                           EXAMPLE_MET_CONTOUR)
