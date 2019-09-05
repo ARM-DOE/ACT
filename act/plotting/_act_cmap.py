070ca0eca104c67dd1159e1c3c5e30bfd3c77f28
ajsockol <ajsockol@ou.edu>
2019-09-05 17:24:46 +0000

    PEP8 changes

--- act/plotting/_act_cmap.py
+++ act/plotting/_act_cmap.py
@@ -7,13 +7,13 @@ import os
 
 
 def yuv_rainbow_24(nc):
-    path1 = np.linspace(0.8*np.pi, 1.8*np.pi, nc)
-    path2 = np.linspace(-0.33*np.pi, 0.33*np.pi, nc)
+    path1 = np.linspace(0.8 * np.pi, 1.8 * np.pi, nc)
+    path2 = np.linspace(-0.33 * np.pi, 0.33 * np.pi, nc)
 
-    y = np.concatenate([np.linspace(0.3, 0.85, nc*2//5),
-                        np.linspace(0.9, 0.0, nc - nc*2//5)])
-    u = 0.40*np.sin(path1)
-    v = 0.55*np.sin(path2) + 0.1
+    y = np.concatenate([np.linspace(0.3, 0.85, nc * 2 // 5),
+                        np.linspace(0.9, 0.0, nc - nc * 2 // 5)])
+    u = 0.40 * np.sin(path1)
+    v = 0.55 * np.sin(path2) + 0.1
 
     rgb_from_yuv = np.array([[1, 0, 1.13983],
                              [1, -0.39465, -0.58060],
@@ -22,9 +22,9 @@ def yuv_rainbow_24(nc):
     for i in range(len(y)):
         yuv = np.array([y[i], u[i], v[i]])
         rgb = rgb_from_yuv.dot(yuv)
-        red_tuple = (i/(len(y)-1.0), rgb[0], rgb[0])
-        green_tuple = (i/(len(y)-1.0), rgb[1], rgb[1])
-        blue_tuple = (i/(len(y)-1.0), rgb[2], rgb[2])
+        red_tuple = (i / (len(y) - 1.0), rgb[0], rgb[0])
+        green_tuple = (i / (len(y) - 1.0), rgb[1], rgb[1])
+        blue_tuple = (i / (len(y) - 1.0), rgb[2], rgb[2])
         cmap_dict['blue'].append(blue_tuple)
         cmap_dict['red'].append(red_tuple)
         cmap_dict['green'].append(green_tuple)
@@ -41,8 +41,7 @@ def yuv_rainbow_24(nc):
 # HomeyerRainbow developed by Cameron Homeyer with assistance from Bobby Jackson
 
 
-
 data_dir = os.path.split(__file__)[0]
 bal_rgb_vals = np.genfromtxt(os.path.join(data_dir, 'balance-rgb.txt'))
 
-datad = {'HomeyerRainbow': yuv_rainbow_24(15), 'balance':bal_rgb_vals}
+datad = {'HomeyerRainbow': yuv_rainbow_24(15), 'balance': bal_rgb_vals}
