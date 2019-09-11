64ad35276f1d68bb311ba500051a6b3513eecf7e
Corey Godine <cgodine@ou.edu>
2019-09-11 19:32:40 +0000

    adding new retrieval to __init__.py

--- act/retrievals/__init__.py
+++ act/retrievals/__init__.py
@@ -12,7 +12,9 @@ This module contains various retrievals for ARM datsets.
 
     calculate_stability_indicies
     generic_sobel_cbh
+    calculate_precipitable_water
 """
 
 from .stability_indices import calculate_stability_indicies
 from .cbh import generic_sobel_cbh
+from .pwv_calc import calculate_precipitable_water
