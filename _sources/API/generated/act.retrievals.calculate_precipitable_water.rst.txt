2d3a975dc29fc956d9c1e13b2e288c07a040e2c7
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2019-09-11 20:03:35 +0000

    Update docs after building Travis build 268 of ANL-DIGR/ACT
    The docs were built from the branch 'master' against the commit
    619507718da661e34040ee4d9aee52fe7a36bee8.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/ANL-DIGR/ACT/jobs/583832558.
    
    The doctr command that was run is
    
        /home/travis/miniconda3/envs/testenv/bin/doctr deploy .

---
+++ _sources/API/generated/act.retrievals.calculate_precipitable_water.rst.txt
@@ -0,0 +1,6 @@
+act.retrievals.calculate\_precipitable\_water
+=============================================
+
+.. currentmodule:: act.retrievals
+
+.. autofunction:: calculate_precipitable_water
--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.3.post8+g827cdb4',
+    VERSION: '0.1.3.post13+g6195077',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
