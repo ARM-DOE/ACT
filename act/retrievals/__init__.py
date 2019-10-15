4f6f91c93dae08a0ea87d84f43d7e229a27283d6
Kenneth Kehoe <kkehoe@ou.edu>
2019-10-15 10:30:45 -0600

    Adding doppler_lidar

--- act/retrievals/__init__.py
+++ act/retrievals/__init__.py
@@ -18,3 +18,4 @@ This module contains various retrievals for ARM datsets.
 from .stability_indices import calculate_stability_indicies
 from .cbh import generic_sobel_cbh
 from .pwv_calc import calculate_precipitable_water
+from .doppler_lidar import compute_winds_from_ppi
