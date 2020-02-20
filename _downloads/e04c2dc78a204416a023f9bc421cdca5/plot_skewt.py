ce5353fe704cf1a8c7065a097b470ff714e7b586
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2020-02-20 19:44:39 +0000

    Update docs after building Travis build 479 of ARM-DOE/ACT
    The docs were built from the branch 'master' against the commit
    372c88530e97d08ff37f6d8959a0d0b2a3f0d820.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/ARM-DOE/ACT/jobs/653145737.
    
    The doctr command that was run is
    
        /home/travis/miniconda3/envs/testenv/bin/doctr deploy .

--- _downloads/e04c2dc78a204416a023f9bc421cdca5/plot_skewt.py
+++ _downloads/e04c2dc78a204416a023f9bc421cdca5/plot_skewt.py
@@ -2,23 +2,28 @@
 Example on how to plot a Skew-T plot of a sounding
 --------------------------------------------------
 
-This example shows how to make a Skew-T plot from a sounding.
-"""
-import act
-import numpy as np
+This example shows how to make a Skew-T plot from a sounding
+and calculate stability indicies.
 
+"""
+
+
+import act
 from matplotlib import pyplot as plt
 
+# Read data
 sonde_ds = act.io.armfiles.read_netcdf(
     act.tests.sample_files.EXAMPLE_SONDE1)
 
-skewt = act.plotting.SkewTDisplay(sonde_ds)
-
-skewt.plot_from_u_and_v('u_wind', 'v_wind', 'pres', 'tdry', 'dp')
-
-plt.show(skewt.fig)
-
+# Calculate stability indicies
 sonde_ds = act.retrievals.calculate_stability_indicies(
     sonde_ds, temp_name="tdry", td_name="dp", p_name="pres")
 print(sonde_ds["lifted_index"])
+
+# Set up plot
+skewt = act.plotting.SkewTDisplay(sonde_ds, figsize=(15, 10))
+
+# Add data
+skewt.plot_from_u_and_v('u_wind', 'v_wind', 'pres', 'tdry', 'dp')
 sonde_ds.close()
+plt.show()
