57bfc7af327d048bc899820feada7c1491afa7c8
Jenni Kyrouac <jkyrouac@anl.gov>
2020-03-04 10:24:24 -0500

    Modify datetime_utils.py to conform to pep8

--- act/utils/datetime_utils.py
+++ act/utils/datetime_utils.py
@@ -160,8 +160,8 @@ def datetime64_to_datetime(time):
 
     datetime_array = []
     for i in range(0, len(time)):
-        timestamp = ((time[i] - np.datetime64('1970-01-01T00:00:00'))
-                     / np.timedelta64(1, 's'))
+        timestamp = ((time[i] - np.datetime64('1970-01-01T00:00:00')) /
+                     np.timedelta64(1, 's'))
         datetime_format = dt.datetime.utcfromtimestamp(timestamp)
         datetime_array.append(datetime_format)
 
