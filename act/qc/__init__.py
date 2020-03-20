0e7c42b614adaf33e48d3fdf5133cc96b3243851
Adam Theisen <atheisen@anl.gov>
2020-03-20 16:08:45 -0500

    ADD: FFT tests for radiometers

--- act/qc/__init__.py
+++ act/qc/__init__.py
@@ -40,9 +40,11 @@ and for applying tests to data
     qcfilter.QCFilter.unset_test
     qcfilter.QCFilter.update_ancillary_variable
     qctests.QCTests
+    radiation_tests.fft_shading_test
 """
 
 from . import qcfilter
 from . import qctests, comparison_tests
 from . import clean
 from . import arm
+from . import radiation_tests
