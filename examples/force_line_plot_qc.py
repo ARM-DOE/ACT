28d9980f8618cb9cbcf3ca4133f7ed295997292c
Kenneth Kehoe <kkehoe@ou.edu>
2020-05-27 21:23:57 -0600

    Fixing some metadata.

--- examples/force_line_plot_qc.py
+++ examples/force_line_plot_qc.py
@@ -4,13 +4,13 @@ Example for working with embedded quality control variables
 ===========================================================
 
 This is an example of how to use 2 dimentional DataArrays containing
-multiple 1 dimentional data including a summary quality control
+multiple 1 dimentional data, including a summary quality control
 plot showing the tests for each 1 dimentional data slice. This
 example uses the force_line_plot keyword which will force multiple
 1 dimentional plots for each element along the second dimention of
 the non-time dimention.
 
-Author: Adam Theisen
+Author: Ken Kehoe
 
 """
 
@@ -20,7 +20,7 @@ from act.plotting import TimeSeriesDisplay
 from act.tests import EXAMPLE_SURFSPECALB1MLAWER
 
 # Read a data file that has a 2D DataArray of multiple 1D data.
-# The corresponding qualit control DataArray is also read in and
+# The corresponding quality control DataArray is also read in and
 # will be used to make a summary plot of quality control infomation
 # of each assessment category.
 obj = read_netcdf(EXAMPLE_SURFSPECALB1MLAWER)
@@ -32,9 +32,9 @@ var_name = 'surface_albedo_mfr_narrowband_10m'
 # vertical plots of 800 by 400 pixels.
 display = TimeSeriesDisplay(obj, subplot_shape=(2, ), figsize=(8, 2 * 4))
 
-# Create the top plot of data using the force_line_plot=True option.
+# Create the top plot of data using the force_line_plot option.
 # This will force the plotting to not assume the data are 2D data that
-# would normally be plotted as a 2D plot, but for each index into the
+# would normally be plotted as a 2D plot. Rather for each index into the
 # filter dimention plot a 1D time series plot. Setting labels=True
 # will create a legend using the filter dimention DataArray.
 display.plot(var_name, force_line_plot=True, labels=True)
