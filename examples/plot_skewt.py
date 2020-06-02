080468553b0e91585b910a40b5aa1762aa5eedcd
Adam Theisen <atheisen@anl.gov>
2020-06-01 20:13:47 -0500

    BUG: Fixing example if METPY is not available

--- examples/plot_skewt.py
+++ examples/plot_skewt.py
@@ -3,7 +3,8 @@ Example on how to plot a Skew-T plot of a sounding
 --------------------------------------------------
 
 This example shows how to make a Skew-T plot from a sounding
-and calculate stability indicies.
+and calculate stability indicies.  METPy needs to be installed
+in order to run this example
 
 """
 
@@ -11,19 +12,26 @@ and calculate stability indicies.
 import act
 from matplotlib import pyplot as plt
 
-# Read data
-sonde_ds = act.io.armfiles.read_netcdf(
-    act.tests.sample_files.EXAMPLE_SONDE1)
+try:
+    import metpy
+    METPY = True
+except ImportError:
+    METPY = False
 
-# Calculate stability indicies
-sonde_ds = act.retrievals.calculate_stability_indicies(
-    sonde_ds, temp_name="tdry", td_name="dp", p_name="pres")
-print(sonde_ds["lifted_index"])
+if METPY:
+    # Read data
+    sonde_ds = act.io.armfiles.read_netcdf(
+        act.tests.sample_files.EXAMPLE_SONDE1)
 
-# Set up plot
-skewt = act.plotting.SkewTDisplay(sonde_ds, figsize=(15, 10))
+    # Calculate stability indicies
+    sonde_ds = act.retrievals.calculate_stability_indicies(
+        sonde_ds, temp_name="tdry", td_name="dp", p_name="pres")
+    print(sonde_ds["lifted_index"])
 
-# Add data
-skewt.plot_from_u_and_v('u_wind', 'v_wind', 'pres', 'tdry', 'dp')
-sonde_ds.close()
-plt.show()
+    # Set up plot
+    skewt = act.plotting.SkewTDisplay(sonde_ds, figsize=(15, 10))
+
+    # Add data
+    skewt.plot_from_u_and_v('u_wind', 'v_wind', 'pres', 'tdry', 'dp')
+    sonde_ds.close()
+    plt.show()
