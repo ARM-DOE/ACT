f251c4cf51b975f526c77d174012fed252530b98
Adam Theisen <atheisen@anl.gov>
2020-06-24 18:18:13 -0500

    ENH: Updating to use dask to speed up the new multi file reading capability

--- act/io/mpl.py
+++ act/io/mpl.py
@@ -12,8 +12,8 @@ import shutil
 import subprocess
 import tempfile
 
-import pandas
 import xarray as xr
+import dask
 
 from act.io.armfiles import check_arm_standards
 
@@ -34,7 +34,8 @@ def read_sigma_mplv5(filename, save_nc=False, out_nc_path=None, afterpulse=None,
     Parameters
     ----------
     filename : str
-        Name of file to read.
+        Name of file(s) to read.  If multiple, ensure that they are sorted,
+        otherwise they will not concat properly
     save_nc : bool
         Whether or not to save intermediate step nc file.
     out_nc_path : str
@@ -53,14 +54,56 @@ def read_sigma_mplv5(filename, save_nc=False, out_nc_path=None, afterpulse=None,
         raise ImportError(
             'The module mpl2nc is not installed and is needed to read '
             'mpl binary files!')
-    datastream_name = '.'.join(
-        filename.split('/')[-1].split('.')[0:2])
 
-    if '.bin' not in filename:
+    if isinstance(filename, str):
+        filename = [filename]
+
+    task = []
+    for f in filename:
+        task.append(dask.delayed(proc_sigma_mplv5_read)(f, save_nc=save_nc,
+                    out_nc_path=out_nc_path, afterpulse=afterpulse, dead_time=dead_time,
+                    overlap=overlap, **kwargs))
+
+    results = dask.compute(*task)
+
+    obj = xr.concat(results, 'time')
+
+    return obj
+
+
+def proc_sigma_mplv5_read(f, save_nc=False, out_nc_path=None, afterpulse=None,
+                          dead_time=None, overlap=None, **kwargs):
+    """
+    Returns `xarray.Dataset` with stored data and metadata from a user-defined
+    SIGMA MPL V5 files. File is converted to netCDF using mpl2nc an optional
+    dependency.  Dask sub-routine for processing
+
+    Parameters
+    ----------
+    filename : str
+        Name of file to read.
+    save_nc : bool
+        Whether or not to save intermediate step nc file.
+    out_nc_path : str
+        Path to save intermediate step nc file.
+    afterpulse : str
+        File with afterpulse correction (.bin)
+    dead_time : str
+        File with dead time correction (.bin)
+    overlap : str
+        File with overlap correction (.bin)
+    **kwargs : keywords
+        Keywords to pass through to xarray.open_dataset().
+
+    """
+
+    datastream_name = '.'.join(
+        f.split('/')[-1].split('.')[0:2])
+    if '.bin' not in f:
         mpl = True
         tmpfile1 = tempfile.mkstemp(suffix='.bin', dir='.')[1]
-        shutil.copyfile(filename, tmpfile1)
-        filename = tmpfile1
+        shutil.copyfile(f, tmpfile1)
+        f = tmpfile1
     else:
         mpl = False
 
@@ -73,8 +116,9 @@ def read_sigma_mplv5(filename, save_nc=False, out_nc_path=None, afterpulse=None,
     if overlap is not None:
         call += ' -o ' + overlap
 
-    call += ' ' + filename
+    call += ' ' + f
 
+    print(call)
     # Specify the output, will use a temporary file if no output specified
     if save_nc:
         if out_nc_path is None:
