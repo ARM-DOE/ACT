cbed47994beafb649259082db8d47b2a7e10f025
Adam Theisen <atheisen@anl.gov>
2020-08-15 19:28:45 -0500

    ENH: Testing out

--- act/qc/qcfilter.py
+++ act/qc/qcfilter.py
@@ -397,7 +397,7 @@ class QCFilter(qctests.QCTests, comparison_tests.QCTests, object):
 
         qc_var_name = self._obj.qcfilter.check_for_ancillary_qc(var_name)
 
-        qc_variable = self._obj[qc_var_name].values
+        qc_variable = np.array(self._obj[qc_var_name].values)
 
         if index is not None:
             if flag_value:
