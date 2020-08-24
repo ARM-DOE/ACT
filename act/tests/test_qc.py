c5f334de6d14a91c91aebaaeacb9953ab7b9c28c
Kenneth Kehoe <kkehoe@ou.edu>
2020-08-24 17:36:55 -0600

    Adding test for datafilter() method.

--- act/tests/test_qc.py
+++ act/tests/test_qc.py
@@ -243,3 +243,24 @@ def test_qctests():
 
     comp_object.close()
     ds_object.close()
+
+
+def test_datafilter():
+    from act.io.armfiles import read_netcdf
+    from act.tests import EXAMPLE_MET1
+
+    ds = read_netcdf(EXAMPLE_MET1)
+    ds.clean.cleanup()
+
+    var_name = 'atmos_pressure'
+
+    ds_1 = ds.mean()
+
+    ds.qcfilter.add_less_test(var_name, 99, test_assessment='Bad')
+    ds.qcfilter.datafilter(rm_assessments='Bad')
+    ds_2 = ds.mean()
+
+    assert np.isclose(ds_1[var_name].values, 98.86, atol=0.01)
+    assert np.isclose(ds_2[var_name].values, 99.15, atol=0.01)
+
+    ds.close()
