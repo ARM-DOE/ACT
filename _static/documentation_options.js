62bfb3c7a85d4c9ecdc0556cca917726f9f64077
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2021-02-04 23:28:26 +0000

    Update docs after building Travis build 714 of ARM-DOE/ACT
    The docs were built from the branch 'master' against the commit
    39ac0e21da214f8026eb36d1c3377b433958d4a9.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/ARM-DOE/ACT/jobs/757617051.
    
    The doctr command that was run is
    
        /home/travis/miniconda3/envs/testenv/bin/doctr deploy .

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.5.1.post37+gc25c49f',
+    VERSION: '0.5.1.post47+g39ac0e2',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
