3f42ba553ff46bec661579cf96e3d1e22ec35e0f
Adam Theisen <atheisen@anl.gov>
2021-04-13 21:25:02 -0500

    ENH: updating  unit tests

--- act/tests/test_plotting.py
+++ act/tests/test_plotting.py
@@ -598,6 +598,6 @@ def test_plot_barbs_from_u_v2():
 def test_2D_timeseries_plot():
     obj = arm.read_netcdf(sample_files.EXAMPLE_CEIL1)
     display = TimeSeriesDisplay(obj)
-    display.plot('backscatter', y_rng=[0, 5000])
+    display.plot('backscatter', y_rng=[0, 5000],  use_var_for_y='range')
     matplotlib.pyplot.show()
     return display.fig
