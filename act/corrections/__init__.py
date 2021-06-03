bcb7bc5c61800bf515bd058e0d200d543861fbe4
zssherman <shermanx1991@gmail.com>
2021-06-02 19:20:32 -0500

    MNT: Reverting old import, was checking with original errors.

--- act/corrections/__init__.py
+++ act/corrections/__init__.py
@@ -3,10 +3,10 @@ The procedures in this module contain corrections for various datasets.
 
 """
 
-from .ceil import *
-from .mpl import *
-from .ship import *
-from .doppler_lidar import *
-from .raman_lidar import *
+from . import ceil
+from . import mpl
+from . import ship
+from . import doppler_lidar
+from . import raman_lidar
 
 __all__ = [s for s in dir() if not s.startswith('_')]
