84a4e5a93148004ec479345bc46ff4deb0933d62
zssherman <shermanx1991@gmail.com>
2021-06-04 13:29:38 -0500

    STY: PEP8 fix spaces of inline comments.

--- act/corrections/__init__.py
+++ act/corrections/__init__.py
@@ -3,8 +3,8 @@ The procedures in this module contain corrections for various datasets.
 
 """
 
-from .ceil import * # noqa: F403
-from .mpl import * # noqa: F403
-from .ship import * # noqa: F403
-from .doppler_lidar import * # noqa: F403
-from .raman_lidar import * # noqa: F403
+from .ceil import *  # noqa: F403
+from .mpl import *  # noqa: F403
+from .ship import *  # noqa: F403
+from .doppler_lidar import *  # noqa: F403
+from .raman_lidar import *  # noqa: F403
--- act/qc/__init__.py
+++ act/qc/__init__.py
@@ -5,7 +5,7 @@ and for applying tests to data.
 """
 
 from .qcfilter import *  # noqa: F403
-from .qctests import * # noqa: F403
-from .clean import * # noqa: F403
-from .arm import * # noqa: F403
-from .radiometer_tests import * # noqa: F403
+from .qctests import *  # noqa: F403
+from .clean import *  # noqa: F403
+from .arm import *  # noqa: F403
+from .radiometer_tests import *  # noqa: F403
