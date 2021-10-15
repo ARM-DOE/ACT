469d283e150366a87e2c9ac4d4fa66501d6f4f65
Adam Theisen <atheisen@anl.gov>
2021-10-14 21:14:24 -0500

    ENH: Adding a function to calculate PBL fro liu liang paper

--- act/retrievals/sonde.py
+++ act/retrievals/sonde.py
@@ -5,6 +5,9 @@ Functions for radiosonde related calculations.
 
 import warnings
 import numpy as np
+import pandas as pd
+import xarray as xr
+from act.utils.data_utils import convert_to_potential_temp
 
 try:
     from pkg_resources import DistributionNotFound
@@ -233,4 +236,220 @@ def calculate_stability_indicies(ds, temp_name="temperature",
     ds["lifted_condensation_level_pressure"] = lcl[0].magnitude
     ds["lifted_condensation_level_pressure"].attrs['units'] = lcl[0].units
     ds["lifted_condensation_level_pressure"].attrs['long_name'] = "Lifted condensation level pressure"
+    return ds
+
+
+def calculate_pbl_liu_liang(ds, temperature='tdry', pressure='pres', windspeed='wspd', height='alt',
+                            smooth_height=3, land_parameter=True, llj_max_alt=1500., llj_max_wspd=2.):
+    """
+    Function for calculating the PBL height from a radiosonde profile
+    using the Liu-Liang 2010 technique.  There are some slight descrepencies
+    in the function from the ARM implementation 1.) it imposes a 1500m (keyword)
+    height on the definition of the LLJ and 2.) the interpolation is slightly different
+    using python functions
+
+    Parameters
+    ----------
+    ds : xarray Dataset
+        Dataset housing radiosonde profile for calculations
+    temperature : str
+        The name of the temperature field.
+    pressure : str
+        The name of the pressure field.
+    windspeed : str
+        The name of the  wind speed field.
+    height : str
+        The name of the height field
+    smooth_height : int
+        Number of points to do a moving average on sounding height data to reduce noise
+    land_parameter : boolean
+        Set to True if retrievals over land or false to retrievals over water
+    llj_max_alt : float
+        Maximum altitude the LLJ 2 m/s difference should be checked against
+    llj_max_wspd : float
+        Maximum wind speed threshold to use to define LLJ
+
+    Returns
+    -------
+    obj : xarray Dataset
+        xarray dataset with results stored in pblht_liu_liang variable
+
+    References
+    ----------
+    Liu, Shuyan, and Xin-Zhong Liang. "Observed diurnal cycle climatology of planetary
+        boundary layer height." Journal of Climate 23, no. 21 (2010): 5790-5809.
+
+    Sivaraman, C., S. McFarlane, E. Chapman, M. Jensen, T. Toto, S. Liu, and M. Fischer.
+        "Planetary boundary layer (PBL) height value added product (VAP): Radiosonde retrievals.
+        " Department of Energy Office of Science Atmospheric Radiation Measurement (ARM) Program
+        (United States) (2013).
+
+    """
+
+    time_0 = ds['time'].values
+    temp_0 = ds[temperature].values
+
+    ds[pressure] = ds[pressure].rolling(time=smooth_height, min_periods=1, center=True).mean()
+    obj = ds.swap_dims(dims_dict={'time': pressure})
+    for var in obj:
+        obj[var].attrs = ds[var].attrs
+
+    base = 5 # 5 mb base
+    starting_pres = base * np.ceil(float(obj[pressure].values[2]) / base)
+    p_grid = np.flip(np.arange(100., starting_pres + base, base))
+
+    try:
+        obj = obj.sel(pres=p_grid, method='nearest')
+    except:
+        ds[pressure] = ds[pressure].rolling(time=smooth_height + 4, min_periods=2, center=True).mean()
+        obj = ds.swap_dims(dims_dict={'time': pressure})
+        for var in obj:
+            obj[var].attrs = ds[var].attrs
+        obj = obj.sel(pres=p_grid, method='nearest')
+
+    # Get Data Variables
+    if smooth_height > 0:
+        alt = pd.Series(obj[height].values).rolling(window=smooth_height, min_periods=0).mean().values
+    else:
+        alt = obj[height].values
+    if np.isnan(alt[0]):
+        idx = np.where(~np.isnan(alt))[0]
+        agl = alt - alt[idx[0]]
+    else:
+        agl = alt - alt[0]
+    pres = obj[pressure].values
+    temp = obj[temperature].values
+    wspd = obj[windspeed].values
+
+    # Perform Pre-processing checks
+    if len(temp) == 0:
+        raise ValueError("No data in profile")
+
+    if np.nanmax(alt) < 1000.:
+        raise ValueError("Max altitude < 1000m")
+
+    if np.nanmax(pres) <= 200.:
+        raise ValueError("Max pressure <= 200 hPa")
+
+    # Check temperature delta
+    t1 = time_0[0]
+    t2 = t1 + np.timedelta64(10, 's')
+    idx = np.where((time_0 >= t1) & (time_0 <= t2))[0]
+    t_delta = abs(temp_0[idx[-1]] - temp_0[idx[0]])
+    if t_delta > 30.:
+        raise ValueError('Temperature changes by >30º in first 10 seconds')
+
+    # Check min/max
+    if np.nanmax(temp) > 50. or np.nanmin(temp) < -90:
+        raise ValueError('Temperature outside acceptable range (-90, 50)')
+
+    if np.isnan(pres[0]) or np.isnan(pres[1]):
+        raise ValueError('First two pressure values bad')
+
+    # Calculate potential temperature and subsequent gradients
+    theta = convert_to_potential_temp(obj=obj, temp_var_name='tdry', press_var_name='pres') + 273.15
+    atts = {'units': 'K', 'long_name': 'Potential temperature'}
+    da = xr.DataArray(theta, coords=obj['tdry'].coords, dims=obj['tdry'].dims, attrs=atts)
+    obj['potential_temperature'] = da
+
+    #theta = obj['potential_temperature'].values
+    theta_diff = theta[4] - theta[1]
+    theta_gradient = np.diff(theta) / np.diff(alt/1000.)
+
+    # Set up threshold values
+    if land_parameter:
+        stability_thresh = 1.0 # K
+        inst_thresh = 0.5 # K
+        overshoot_thresh = 4.0 # K/km
+    else:
+        stability_thresh = 0.2 # K
+        inst_thresh = 0.1 # K
+        overshoot_thresh = 0.5 # K/km
+
+    # Check Regimes
+    if theta_diff < 0 - stability_thresh:
+        regime = 'CBL'
+    if theta_diff > abs(stability_thresh):
+        regime = 'SBL'
+    if (0 - stability_thresh) <= theta_diff <= abs(stability_thresh):
+        regime = 'NRL'
+
+    # Calculate for CBL/NRL regimes
+    if regime == 'CBL' or regime == 'NRL':
+        # Calculate gradient from first level
+        theta_gradient_0 = theta - theta[0]
+
+        # Only process data above 150m ARM
+        idx = np.where(agl > 150)[0][0]
+        theta_gradient_0[0:idx] = np.nan
+
+        # Scan upward to find lowest level that meets condition
+        idx = np.where(theta_gradient_0 >= inst_thresh)[0]
+        theta_gradient[0:idx[0]] = np.nan
+
+        # Scan upward from previous level to search for overlying inversion layer
+        idx = np.where(theta_gradient >= overshoot_thresh)[0]
+        pbl = alt[idx[0]]
+    else:
+        pbl_stable = np.nan
+        pbl_shear = np.nan
+
+        idx = np.array([i for i, t in enumerate(theta_gradient[1:-1]) if theta_gradient[i] < theta_gradient[i-1] and theta_gradient[i] < theta_gradient[i+1]])
+     
+        for i in idx:
+            cond1 = (theta_gradient[i] - theta_gradient[i - 1]) < -40. # Local peak?
+            cond2 = (theta_gradient[i + 1] < overshoot_thresh) or (theta_gradient[i + 2] < overshoot_thresh)
+            if cond1 or cond2:
+                pbl_stable = (alt[i+1] + alt[i]) / 2. # This gets the ARM answer
+                #pbl_stable = alt[i]
+                break
+
+        # Check for low-level jet
+        # Find the height of the maximum windspeed and look up to find layer 2m/s lower
+        # Stull 1988 indicates LLJ is defined as where there is a relative wind speed
+        # maximum that is more than 2 m/s faster than the wind speeds above it within
+        # the lowest 1500m of the atmosphere. Keywords to adjust are provided
+        idh = np.where(alt <= llj_max_alt)[0]
+        max_wspd_ind = [i for i, w in enumerate(wspd[:-1]) if wspd[i] > wspd[i+1]][0]
+        diff = wspd[max_wspd_ind] - wspd[max_wspd_ind:idh[-1]]
+        idx = np.where(diff > llj_max_wspd)[0]
+        if len(idx) > 0:
+            wspd_to_surf = np.diff(np.flip(wspd[0:max_wspd_ind]))
+            wspd_monotonic = np.all(wspd_to_surf <= 0.)
+            if wspd_monotonic:
+                pbl_shear = alt[max_wspd_ind]
+
+        if ~np.all(np.isnan([pbl_stable, pbl_shear])):
+            pbl = np.nanmin([pbl_stable, pbl_shear])
+        else:
+            pbl = -9999.
+
+    atts = {'units': 'm', 'long_name': 'Planteary Boundary Layer Height Liu-Liang'}
+    da = xr.DataArray(pbl, attrs=atts)
+    ds['pblht_liu_liang'] = da
+
+    atts = {'units': '', 'long_name': 'Planteary Boundary Layer Regime Classification Liu-Liang'}
+    da = xr.DataArray(regime, attrs=atts)
+    ds['pblht_regime_liu_liang'] = da
+
+    atts = {'units': 'mb', 'long_name': 'Gridded pressure'}
+    da = xr.DataArray(pres, coords={'atm_pres_ss': pres}, dims=['atm_pres_ss'], attrs=atts)
+    ds['atm_pres_ss'] = da
+
+    atts = {'units': 'K', 'long_name': 'Gridded potential temperature'}
+    da = xr.DataArray(theta, coords={'atm_pres_ss': pres}, dims=['atm_pres_ss'], attrs=atts)
+    ds['potential_temperature_ss'] = da
+
+    atts = {'units': 'm', 'long_name': 'Gridded altitude'}
+    da = xr.DataArray(alt, coords={'atm_pres_ss': pres}, dims=['atm_pres_ss'], attrs=atts)
+    ds['alt_ss'] = da
+
+    atts = {'units': 'm', 'long_name': 'PBL Stable Condition 1'}
+    da = xr.DataArray(pbl_stable, attrs=atts)
+    ds['pblht_liu_liang_stable_cond'] = da
+
+    atts = {'units': 'm', 'long_name': 'PBL Shear Condition 2'}
+    da = xr.DataArray(pbl_shear, attrs=atts)
+    ds['pblht_liu_liang_shear_cond'] = da
+
     return ds
