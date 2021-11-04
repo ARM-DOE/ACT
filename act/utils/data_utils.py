60448527b44d6f5dd7bc57c9bb2fc489b201308c
zssherman <shermanx1991@gmail.com>
2021-11-04 14:20:23 -0500

    FIX: Fix for lat lon issues.

--- act/utils/data_utils.py
+++ act/utils/data_utils.py
@@ -649,9 +649,9 @@ def create_pyart_obj(obj, variables=None, sweep=None, azimuth=None,
     radar.time['data'] = np.array(obj['time'].values)
 
     # Add lat, lon, and alt
-    radar.latitude['data'] = np.array([obj[lat].values])
-    radar.longitude['data'] = np.array([obj[lon].values])
-    radar.altitude['data'] = np.array([obj[alt].values])
+    radar.latitude['data'] = np.array(obj[lat].values)
+    radar.longitude['data'] = np.array(obj[lon].values)
+    radar.altitude['data'] = np.array(obj[alt].values)
 
     # Add sweep information
     radar.sweep_number['data'] = swp
