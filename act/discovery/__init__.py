b3f2508496db76d2faa4de9776b6d00d14e91521
jhemedin <jason.hemedinger@gmail.com>
2022-01-07 12:06:06 -0600

    ADD: get_airnow functionality

--- act/discovery/__init__.py
+++ act/discovery/__init__.py
@@ -7,3 +7,4 @@ ARM Data Discovery.
 from .get_cropscape import croptype
 from .get_armfiles import download_data
 from .get_asos import get_asos
+from .get_airnow import get_AirNow, get_AirNow_obs, get_AirNow_forecast
