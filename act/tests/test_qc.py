df092b3a633bdd37d25a4d26ea940bff3254e7f4
kehoe <kkehoe@ou.edu>
2022-01-12 00:10:34 +0000

    Resolving issue with test having slightly different results between ARM server and GitHub server.

--- act/tests/test_qc.py
+++ act/tests/test_qc.py
@@ -578,7 +578,9 @@ def test_qctests_dos():
     test_meaning = ('Data failing persistence test. Standard Deviation over a '
                     'window of 10 values less than 0.0001.')
     assert ds_object[qc_var_name].attrs['flag_meanings'][-1] == test_meaning
-    assert np.sum(ds_object[qc_var_name].values) == 1500
+    # There is a precision issue with GitHub testing that makes the number of tests
+    # tripped off by 1. This isclose() option is to account for that.
+    assert np.isclose(np.sum(ds_object[qc_var_name].values), 1500, atol=1)
 
     ds_object.qcfilter.add_persistence_test(var_name, window=10000, prepend_text='DQO')
     test_meaning = ('DQO: Data failing persistence test. Standard Deviation over a window of '
