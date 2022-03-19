48395d58a6e5a19732e728fbc3244c4c3fe7fbce
Max Grover <maxi2312@gmail.com>
2022-03-18 18:36:29 -0600

    Update conf.py (#420)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -128,7 +128,7 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # documentation.
 #
 html_theme_options = {
-    'analytics_id': 'UA-179020619-3',
+    'analytics_id': 'G-304558912',
 }
 
 # Add any paths that contain custom static files (such as style sheets) here,
