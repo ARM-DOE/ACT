bc5fc25f0e1ef0ef44b9fb2031b06415820506b8
Robert Jackson <rjackson@Roberts-MacBook-Pro.local>
2022-03-25 09:44:55 -0500

    ADD: Modified skipif

--- act/tests/test_retrievals.py
+++ act/tests/test_retrievals.py
@@ -1,7 +1,6 @@
 ' Unit tests for the ACT retrievals module. ' ''
 
 import glob
-
 import numpy as np
 import pytest
 import xarray as xr
@@ -240,24 +239,26 @@ def test_calculate_pbl_liu_liang():
         obj = act.retrievals.sonde.calculate_pbl_liu_liang(obj)
 
 
+@pytest.mark.skipif(not PYSP2_AVAILABLE,
+reason="PySP2 is not installed.")
 def test_sp2_waveform_stats():
-    if PYSP2:
-        my_sp2b = act.io.read_sp2(act.tests.EXAMPLE_SP2B)
-        my_ini = act.tests.EXAMPLE_INI
-        my_binary = act.qc.get_waveform_statistics(my_sp2b, my_ini, parallel=False)
-        assert my_binary.PkHt_ch1.max() == 62669.4
-        np.testing.assert_almost_equal(np.nanmax(my_binary.PkHt_ch0.values), 98708.92915295, decimal=1)
-        np.testing.assert_almost_equal(np.nanmax(my_binary.PkHt_ch4.values), 54734.05714286, decimal=1)
+    my_sp2b = act.io.read_sp2(act.tests.EXAMPLE_SP2B)
+    my_ini = act.tests.EXAMPLE_INI
+    my_binary = act.qc.get_waveform_statistics(my_sp2b, my_ini, parallel=False)
+    assert my_binary.PkHt_ch1.max() == 62669.4
+    np.testing.assert_almost_equal(np.nanmax(my_binary.PkHt_ch0.values), 98708.92915295, decimal=1)
+    np.testing.assert_almost_equal(np.nanmax(my_binary.PkHt_ch4.values), 54734.05714286, decimal=1)
 
 
+@pytest.mark.skipif(not PYSP2_AVAILABLE,
+reason="PySP2 is not installed.")
 def test_sp2_psds():
-    if PYSP2:
-        my_sp2b = act.io.read_sp2(act.tests.EXAMPLE_SP2B)
-        my_ini = act.tests.EXAMPLE_INI
-        my_binary = act.qc.get_waveform_statistics(my_sp2b, my_ini, parallel=False)
-        my_hk = act.io.read_hk_file(act.tests.EXAMPLE_HK)
-        my_binary = act.retrievals.calc_sp2_diams_masses(my_binary)
-        ScatRejectKey = my_binary['ScatRejectKey'].values
-        assert np.nanmax(my_binary['ScatDiaBC50'].values[ScatRejectKey == 0]) < 1000.0
-        my_psds = act.retrievals.process_sp2_psds(my_binary, my_hk, my_ini)
-        np.testing.assert_almost_equal(my_psds['NumConcIncan'].max(), 0.95805343)
+    my_sp2b = act.io.read_sp2(act.tests.EXAMPLE_SP2B)
+    my_ini = act.tests.EXAMPLE_INI
+    my_binary = act.qc.get_waveform_statistics(my_sp2b, my_ini, parallel=False)
+    my_hk = act.io.read_hk_file(act.tests.EXAMPLE_HK)
+    my_binary = act.retrievals.calc_sp2_diams_masses(my_binary)
+    ScatRejectKey = my_binary['ScatRejectKey'].values
+    assert np.nanmax(my_binary['ScatDiaBC50'].values[ScatRejectKey == 0]) < 1000.0
+    my_psds = act.retrievals.process_sp2_psds(my_binary, my_hk, my_ini)
+    np.testing.assert_almost_equal(my_psds['NumConcIncan'].max(), 0.95805343)
