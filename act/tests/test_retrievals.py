44165deaad5e94ba297caf80cad1f19c3716d417
Robert Jackson <rjackson@Roberts-MacBook-Pro.local>
2022-03-25 11:50:22 -0500

    FIX: PYSP2_AVAILABLE to PYSP2

--- act/tests/test_retrievals.py
+++ act/tests/test_retrievals.py
@@ -239,7 +239,7 @@ def test_calculate_pbl_liu_liang():
         obj = act.retrievals.sonde.calculate_pbl_liu_liang(obj)
 
 
-@pytest.mark.skipif(not PYSP2_AVAILABLE,
+@pytest.mark.skipif(not PYSP2,
 reason="PySP2 is not installed.")
 def test_sp2_waveform_stats():
     my_sp2b = act.io.read_sp2(act.tests.EXAMPLE_SP2B)
@@ -250,7 +250,7 @@ def test_sp2_waveform_stats():
     np.testing.assert_almost_equal(np.nanmax(my_binary.PkHt_ch4.values), 54734.05714286, decimal=1)
 
 
-@pytest.mark.skipif(not PYSP2_AVAILABLE,
+@pytest.mark.skipif(not PYSP2,
 reason="PySP2 is not installed.")
 def test_sp2_psds():
     my_sp2b = act.io.read_sp2(act.tests.EXAMPLE_SP2B)
