1ce0011ddea19fe4222a6f1c3ae0f90856a82bb8
Max Grover <maxi2312@gmail.com>
2022-04-04 09:56:09 -0600

    Add pydata theme, similar to PyART (#442)
    * fix formatting errors, add new doc theme
    
    * fix missing blog section, update broken image
    
    * add missing extension

---
+++ examples/plot_get_stability_indices_example.py
@@ -0,0 +1,38 @@
+"""
+Retrieve stability indicies from a sounding
+-------------------------------------------------------------
+
+This example shows how to retrieve CAPE, CIN, and lifted index
+from a sounding.
+"""
+
+import warnings
+
+import act
+
+warnings.filterwarnings('ignore')
+
+
+def print_summary(obj, variables):
+    for var_name in variables:
+        print(f'{var_name}: {obj[var_name].values} ' f"units={obj[var_name].attrs['units']}")
+    print()
+
+
+sonde_ds = act.io.armfiles.read_netcdf(act.tests.sample_files.EXAMPLE_SONDE1)
+
+sonde_ds = act.retrievals.calculate_stability_indicies(
+    sonde_ds, temp_name='tdry', td_name='dp', p_name='pres', rh_name='rh'
+)
+
+variables = [
+    'lifted_index',
+    'surface_based_cape',
+    'surface_based_cin',
+    'most_unstable_cape',
+    'most_unstable_cin',
+    'lifted_condensation_level_temperature',
+    'lifted_condensation_level_pressure',
+]
+
+print_summary(sonde_ds, variables)
