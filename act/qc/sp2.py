3b6f2b8336c01b0c25e86299f23fb3cdfd313bd3
mgrover1 <maxi2312@gmail.com>
2022-04-05 12:16:54 -0500

    fix init of pysp2 qc object

--- act/qc/sp2.py
+++ act/qc/sp2.py
@@ -1,15 +1,18 @@
 import warnings
-import pandas as pd
+
 import numpy as np
+import pandas as pd
 
 try:
     import pysp2
+
     PYSP2_AVAILABLE = True
 except ImportError:
     PYSP2_AVAILABLE = False
 
 if PYSP2_AVAILABLE:
-    class SP2ParticleCriteria(object):
+
+    class SP2ParticleCriteria:
         """
         This class stores the particle crtiteria for filtering out bad particles in the SP2.
         In addition, this class stores the calibration statistics for the mass calculations.
@@ -64,51 +67,52 @@ if PYSP2_AVAILABLE:
 
         """
 
-        def __init__(self, cal_file=None):
-            def __init__(self, cal_file_name=None):
-                self.ScatMaxPeakHt1 = 60000
-                self.ScatMinPeakHt1 = 250
-                self.ScatMaxPeakHt2 = 60000
-                self.ScatMinPeakHt2 = 250
-                self.ScatMinWidth = 10
-                self.ScatMaxWidth = 90
-                self.ScatMinPeakPos = 20
-                self.ScatMaxPeakPos = 90
-                self.IncanMinPeakHt1 = 200
-                self.IncanMinPeakHt2 = 200
-                self.IncanMaxPeakHt1 = 60000
-                self.IncanMaxPeakHt2 = 60000
-                self.IncanMinWidth = 5
-                self.IncanMaxWidth = np.inf
-                self.IncanMinPeakPos = 20
-                self.IncanMaxPeakPos = 90
-                self.IncanMinPeakRatio = 0.1
-                self.IncanMaxPeakRatio = 25
-                self.IncanMaxPeakOffset = 11
-                self.c0Mass1 = 0
-                self.c1Mass1 = 0.0001896
-                self.c2Mass1 = 0
-                self.c3Mass1 = 0
-                self.c0Mass2 = 0
-                self.c1Mass2 = 0.0016815
-                self.c2Mass2 = 0
-                self.c3Mass2 = 0
-                self.c0Scat1 = 0
-                self.c1Scat1 = 78.141
-                self.c2Scat1 = 0
-                self.c0Scat2 = 0
-                self.c1Scat2 = 752.53
-                self.c2Scat2 = 0
-                self.densitySO4 = 1.8
-                self.densityBC = 1.8
-                self.TempSTP = 273.15
-                self.PressSTP = 1013.25
-                if cal_file_name is not None:
-                    df = pd.read_csv(cal_file_name, sep='\t')
-                    for i in range(len(df["CalName"].values)):
-                        setattr(self, df["CalName"].values[i], df["CalValue"].values[i])
-                    del df
+        def __init__(self, cal_file_name=None):
+            self.ScatMaxPeakHt1 = 60000
+            self.ScatMinPeakHt1 = 250
+            self.ScatMaxPeakHt2 = 60000
+            self.ScatMinPeakHt2 = 250
+            self.ScatMinWidth = 10
+            self.ScatMaxWidth = 90
+            self.ScatMinPeakPos = 20
+            self.ScatMaxPeakPos = 90
+            self.IncanMinPeakHt1 = 200
+            self.IncanMinPeakHt2 = 200
+            self.IncanMaxPeakHt1 = 60000
+            self.IncanMaxPeakHt2 = 60000
+            self.IncanMinWidth = 5
+            self.IncanMaxWidth = np.inf
+            self.IncanMinPeakPos = 20
+            self.IncanMaxPeakPos = 90
+            self.IncanMinPeakRatio = 0.1
+            self.IncanMaxPeakRatio = 25
+            self.IncanMaxPeakOffset = 11
+            self.c0Mass1 = 0
+            self.c1Mass1 = 0.0001896
+            self.c2Mass1 = 0
+            self.c3Mass1 = 0
+            self.c0Mass2 = 0
+            self.c1Mass2 = 0.0016815
+            self.c2Mass2 = 0
+            self.c3Mass2 = 0
+            self.c0Scat1 = 0
+            self.c1Scat1 = 78.141
+            self.c2Scat1 = 0
+            self.c0Scat2 = 0
+            self.c1Scat2 = 752.53
+            self.c2Scat2 = 0
+            self.densitySO4 = 1.8
+            self.densityBC = 1.8
+            self.TempSTP = 273.15
+            self.PressSTP = 1013.25
+            if cal_file_name is not None:
+                df = pd.read_csv(cal_file_name, sep='\t')
+                for i in range(len(df['CalName'].values)):
+                    setattr(self, df['CalName'].values[i], df['CalValue'].values[i])
+                del df
+
 else:
+
     class SP2ParticleCriteria:
         def __init__(self):
             warnings.warn(
