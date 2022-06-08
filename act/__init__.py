e028f27322b34665ef06737fef706a5b034e2315
mgrover1 <maxi2312@gmail.com>
2022-06-08 10:50:29 -0500

    remove long list of imports

--- act/__init__.py
+++ act/__init__.py
@@ -7,7 +7,6 @@ ACT: The Atmospheric Community Toolkit
 # No more pandas warnings
 from pandas.plotting import register_matplotlib_converters
 
-from . import corrections, discovery, io, plotting, qc, retrievals, tests, utils
 from ._version import get_versions
 
 register_matplotlib_converters()
