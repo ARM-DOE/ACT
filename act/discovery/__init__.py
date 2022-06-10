873caccedfc92324178f1db6bf00a032ed9bac15
Jason Hemedinger <jason.hemedinger@gmail.com>
2022-06-10 16:38:54 -0500

    ADD: get_airnow functionality (#380)
    * ADD: get_airnow functionality
    
    * ENH: Updating airnow scripts and adding tests
    
    * ENH: PEP8 updates
    
    * ENH: Reverting back
    
    * ENH: Updating based on feedback
    
    Co-authored-by: Adam Theisen <atheisen@anl.gov>

--- act/discovery/__init__.py
+++ act/discovery/__init__.py
@@ -1,9 +1,11 @@
 """
-This module contains procedures for exploring and downloading data on
-ARM Data Discovery.
+This module contains procedures for exploring and downloading data
+from a variety of web services
 
 """
 
 from .get_armfiles import download_data
 from .get_asos import get_asos
 from .get_cropscape import croptype
+from .get_airnow import get_airnow_bounded_obs, get_airnow_obs, get_airnow_forecast
+
