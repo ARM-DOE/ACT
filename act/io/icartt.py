ed0f9a4db79ae3e0ba1353ffe686f8c301dbbf6a
jrobrien91 <obrienj@anl.gov>
2022-07-26 16:08:34 -0500

    ENH: ICARTT Input Completed and Successfully Read into Xarray Dataset.

--- act/io/icartt.py
+++ act/io/icartt.py
@@ -95,5 +95,89 @@ def read_icartt(
     # Define variables
     var = [x for x in ict.variables]
 
+    # Define the Uncertainty for each variable. Note it may not be calculated.
+    # If not calculated, assign 'N/A' to the attribute
+    uncertainty = ict.normalComments[6].split(':')[1].split(',')
+
+    # Define the Upper and Lower Limit of Detection Flags
+    ulod_flag = ict.normalComments[7].split(':')[1]
+    ulod_value = ict.normalComments[8].split(':')[1].split(',')
+    llod_flag = ict.normalComments[9].split(':')[1]
+    llod_value = ict.normalComments[10].split(':')[1].split(',')
+
+    # Convert ICARTT Object to Xarray Dataset
+    ds_container = []
+    # Counter for uncertainty/LOD values
+    counter = 0
+
+    # Loop over ICART variables, convert to Xarray DataArray, Append.
+    for key in ict.variables:
+        #print(key, counter, ulod_value[counter], llod_value[counter])
+        # Note time is the only independent variable within ICARTT
+        # Short name for time must be "Start_UTC" for ICARTT files.
+        if key != 'Start_UTC':
+            da = xr.DataArray(ict.data[key],
+                              coords=dict(time=ict.times),
+                              name=key, dims=['time'])
+            # Assume if Uncertainity does not match the number of variables,
+            # values were not set within the file. Needs to be string!
+            if len(uncertainty) != len(ict.variables):
+                da.attrs['uncertainty'] = 'N/A'
+            else:
+                da.attrs['uncertainty'] = uncertainty[counter]
+
+            # Assume if ULOD does not match the number of variables within the
+            # the file, ULOD values were not set.
+            if len(ulod_value) != len(ict.variables):
+                da.attrs['ULOD_Value'] = 'N/A'
+            else:
+                da.attrs['ULOD_Value'] = ulod_value[counter]
+
+            # Assume if LLOD does not match the number of variables within the
+            # the file, LLOD values were not set.
+            if len(llod_value) != len(ict.variables):
+                da.attrs['LLOD_Value'] = 'N/A'
+            else:
+                da.attrs['LLOD_Value'] = llod_value[counter]
+            # Define the meta data:
+            da.attrs['units'] = ict.variables[key].units
+            da.attrs['mvc'] = ict.variables[key].miss
+            da.attrs['scale_factor'] = ict.variables[key].scale
+            da.attrs['ULOD_Flag'] = ulod_flag
+            da.attrs['LLOD_Flag'] = llod_flag
+            # Append to ds container
+            ds_container.append(da.to_dataset(name=key))
+            # up the counter
+            counter += 1
+
+    # Concatenate each of the Xarray DataArrays into a single Xarray DataSet
+    ds = xr.merge(ds_container)
+
+    # Define Time attributes
+    ds.time.attrs.update(units='Number of Seconds from 00:00:00 UTC')
+
+    # Assign ICARTT Meta data to Xarray DataSet
+    ds.attrs['PI'] = ict.PIName
+    ds.attrs['PI_Affiliation'] = ict.PIAffiliation
+    ds.attrs['Platform'] = ict.dataSourceDescription
+    ds.attrs['Mission'] = ict.missionName
+    ds.attrs['DateOfCollection'] = ict.dateOfCollection
+    ds.attrs['DateOfRevision'] = ict.dateOfRevision
+    ds.attrs['Data_Interval'] = ict.dataIntervalCode
+    ds.attrs['Independent_Var'] = ict.independentVariable
+    ds.attrs['Dependent_Var_Num'] = len(ict.dependentVariables)
+    ds.attrs['PI_Contact'] = ict.normalComments[0].split('\n')[0].split(':')[-1]
+    ds.attrs['Platform'] = ict.normalComments[1].split(':')[-1]
+    ds.attrs['Location'] = ict.normalComments[2].split(':')[-1]
+    ds.attrs['Associated_Data'] = ict.normalComments[3].split(':')[-1]
+    ds.attrs['Instrument_Info'] = ict.normalComments[4].split(':')[-1]
+    ds.attrs['Data_Info'] = ict.normalComments[5][11:]
+    ds.attrs['DM_Contact'] = ict.normalComments[11].split(':')[-1]
+    ds.attrs['Project_Info'] = ict.normalComments[12].split(':')[-1]
+    ds.attrs['Stipulations'] = ict.normalComments[13].split(':')[-1]
+    ds.attrs['Comments'] = ict.normalComments[14].split(':')[-1]
+    ds.attrs['Revision'] = ict.normalComments[15].split(':')[-1]
+    ds.attrs['Revision_Comments'] = ict.normalComments[15+1].split(':')[-1]
+
     # Return Xarray Dataset
-    return ict, var
+    return ict, var, ds
