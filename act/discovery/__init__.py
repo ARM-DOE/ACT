dfc4adbde220373214111cea20d3ee073087555b
mgrover1 <mgroverwx@gmail.com>
2022-10-07 15:32:06 -0600

    fix issues in discovery init

--- act/discovery/__init__.py
+++ act/discovery/__init__.py
@@ -8,10 +8,11 @@ import lazy_loader as lazy
 
 __getattr__, __dir__, __all__ = lazy.attach(
     __name__,
-    submodules=['get_armfiles', 'get_cropscape'],
+    submodules=['get_armfiles', 'get_cropscape', 'get_cropscape', 'get_noaa_psl'],
     submod_attrs={
         'get_armfiles': ['download_data'],
         'get_asos': ['get_asos'],
+        'get_airnow': ['get_airnow_bounded_obs', 'get_airnow_obs', 'get_airnow_forecast'],
         'get_cropscape': ['croptype'],
         'get_noaapsl': ['download_noaa_psl_data'],
     },
