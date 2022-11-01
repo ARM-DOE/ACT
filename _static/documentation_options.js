933302908caeb4dbfefe31eacd22ce00d1f5c62a
mgrover1 <mgrover1@users.noreply.github.com>
2022-11-01 19:27:30 +0000

    deploy: 44aa7d7e7be164440ecc12f341df4545aa3996a2

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.post1+gfa578d4',
+    VERSION: '0.post1+g44aa7d7',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
