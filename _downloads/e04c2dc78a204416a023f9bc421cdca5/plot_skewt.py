02b7deee1e6aa13df7c6455ea2b267c9f736d190
zssherman <zssherman@users.noreply.github.com>
2022-11-20 17:08:50 +0000

    deploy: 0d68d375e7f60ea042cd23abb6fedaa36f46cd37

--- _downloads/e04c2dc78a204416a023f9bc421cdca5/plot_skewt.py
+++ _downloads/e04c2dc78a204416a023f9bc421cdca5/plot_skewt.py
@@ -3,42 +3,34 @@ Skew-T plot of a sounding
 -------------------------
 
 This example shows how to make a Skew-T plot from a sounding
-and calculate stability indicies.  METPy needs to be installed
-in order to run this example
+and calculate stability indicies.
 
 """
 
 
-import xarray as xr
 from matplotlib import pyplot as plt
+import metpy
+import xarray as xr
 
 import act
 
 # Make sure attributes are retained
 xr.set_options(keep_attrs=True)
 
-try:
-    import metpy
+# Read data
+sonde_ds = act.io.armfiles.read_netcdf(act.tests.sample_files.EXAMPLE_SONDE1)
 
-    METPY = True
-except ImportError:
-    METPY = False
+print(list(sonde_ds))
+# Calculate stability indicies
+sonde_ds = act.retrievals.calculate_stability_indicies(
+    sonde_ds, temp_name='tdry', td_name='dp', p_name='pres', rh_name='rh'
+)
+print(sonde_ds['lifted_index'])
 
-if METPY:
-    # Read data
-    sonde_ds = act.io.armfiles.read_netcdf(act.tests.sample_files.EXAMPLE_SONDE1)
+# Set up plot
+skewt = act.plotting.SkewTDisplay(sonde_ds, figsize=(15, 10))
 
-    print(list(sonde_ds))
-    # Calculate stability indicies
-    sonde_ds = act.retrievals.calculate_stability_indicies(
-        sonde_ds, temp_name='tdry', td_name='dp', p_name='pres', rh_name='rh'
-    )
-    print(sonde_ds['lifted_index'])
-
-    # Set up plot
-    skewt = act.plotting.SkewTDisplay(sonde_ds, figsize=(15, 10))
-
-    # Add data
-    skewt.plot_from_u_and_v('u_wind', 'v_wind', 'pres', 'tdry', 'dp')
-    sonde_ds.close()
-    plt.show()
+# Add data
+skewt.plot_from_u_and_v('u_wind', 'v_wind', 'pres', 'tdry', 'dp')
+sonde_ds.close()
+plt.show()
--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.post1+g9bb2a10',
+    VERSION: '0.post1+g0d68d37',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
