69125f35c3d45266712a945aa8181048eab863fe
Adam Theisen <atheisen@anl.gov>
2022-11-21 10:24:16 -0600

    ENH: Removing imports and adding in qctests/filter to the lazy_loading

--- act/qc/__init__.py
+++ act/qc/__init__.py
@@ -8,8 +8,6 @@ import lazy_loader as lazy
 
 # We need to import clean first to register the accessor
 from .clean import *
-from .qcfilter import QCFilter
-from .qctests import QCTests
 
 __getattr__, __dir__, __all__ = lazy.attach(
     __name__,
@@ -19,12 +17,21 @@ __getattr__, __dir__, __all__ = lazy.attach(
         'bsrn_tests',
         'comparison_tests',
         'qcfilter',
+        'qctests',
         'radiometer_tests',
         'sp2',
     ],
     submod_attrs={
         'arm': ['add_dqr_to_qc'],
-        'qcfilter': ['set_bit', 'unset_bit', 'parse_bit'],
+        'qcfilter': [
+            'QCFilter',
+            'parse_bit',
+            'set_bit',
+            'unset_bit',
+        ],
+        'qctests': [
+            'QCTests',
+        ],
         'radiometer_tests': [
             'fft_shading_test',
             'fft_shading_test_process',
