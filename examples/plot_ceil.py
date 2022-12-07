27484bd162ac3da0f327e63dbbae2efc6dbb853f
Adam Theisen <atheisen@anl.gov>
2022-12-06 19:18:18 -0600

    ENH: removing  prinnt`

--- examples/plot_ceil.py
+++ examples/plot_ceil.py
@@ -24,7 +24,6 @@ else:
     act.discovery.download_data(username, token, 'sgpceilC1.b1', '2017-01-14', '2017-01-19')
     ceil_ds = act.io.armfiles.read_netcdf('./sgpceilC1.b1/*')
 
-print(ceil_ds['time'].attrs)
 ceil_ds = act.corrections.ceil.correct_ceil(ceil_ds, -9999.0)
 display = act.plotting.TimeSeriesDisplay(ceil_ds, subplot_shape=(1,), figsize=(15, 5))
 display.plot('backscatter', subplot_index=(0,))
