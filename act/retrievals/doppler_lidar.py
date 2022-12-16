a0985d18f2d3bf071999a48ed339290f3426382f
Adam Theisen <atheisen@anl.gov>
2022-12-16 09:31:06 -0600

    ENH: Converting doppler lidar code to use dask

--- act/retrievals/doppler_lidar.py
+++ act/retrievals/doppler_lidar.py
@@ -3,7 +3,7 @@ Functions for doppler lidar specific retrievals
 
 """
 import warnings
-
+import dask
 import numpy as np
 import xarray as xr
 
@@ -73,6 +73,7 @@ def compute_winds_from_ppi(
     Techniques Discussions 2016, 10, 1-30
 
     """
+
     azimuth = obj[azimuth_name].values
     azimuth_rounded = np.round(azimuth).astype(int)
 
@@ -90,196 +91,218 @@ def compute_winds_from_ppi(
     else:
         num_scans = index[1] - index[0]
 
+    elevation = np.radians(obj[elevation_name].values)
+    azimuth = np.radians(obj[azimuth_name].values)
+    doppler = obj[radial_velocity_name].values
+    if intensity_name is not None:
+        intensity = obj[intensity_name].values
+        snr = intensity - 1
+        del intensity
+        var_name = intensity_name
+    else:
+        try:
+            snr = obj[snr_name].values
+        except KeyError:
+            intensity = obj['intensity'].values
+            snr = intensity - 1
+            del intensity
+            var_name = 'intensity'
+
+    height_name = list(set(obj[var_name].dims) - {'time'})[0]
+    rng = obj[height_name].values
+    try:
+        height_units = obj[height_name].attrs['units']
+    except KeyError:
+        if rng[0] > 0:
+            height_units = 'm'
+        else:
+            height_units = 'km'
+    time = obj['time'].values
+
     # Loop over each PPI scan
+    task = []
     for start_index in index:
         scan_index = range(start_index, start_index + num_scans)
         # Since this can run while instrument is making measurements
         # the number of PPI scans may not match exactly. This will
         # adjust the number of scans in case there is an issue.
-        if scan_index[-1] > obj[elevation_name].values.size:
-            scan_index = range(start_index, obj[elevation_name].values.size)
+        if scan_index[-1] > np.size(elevation):
+            scan_index = range(start_index, np.size(elevation))
 
-        elevation = np.radians(obj[elevation_name].values[scan_index])
-        azimuth = np.radians(obj[azimuth_name].values[scan_index])
-        doppler = obj[radial_velocity_name].values[scan_index, :]
-        if intensity_name is not None:
-            intensity = obj[intensity_name].values[scan_index, :]
-            snr = intensity - 1
-            del intensity
-            var_name = intensity_name
-        else:
-            try:
-                snr = obj[snr_name].values[scan_index, :]
-            except KeyError:
-                intensity = obj['intensity'].values[scan_index, :]
-                snr = intensity - 1
-                del intensity
-                var_name = 'intensity'
-
-        height_name = list(set(obj[var_name].dims) - {'time'})[0]
-        rng = obj[height_name].values
-        try:
-            height_units = obj[height_name].attrs['units']
-        except KeyError:
-            if rng[0] > 0:
-                height_units = 'm'
-            else:
-                height_units = 'km'
-        time = obj['time'].values[scan_index]
-
-        height = rng * np.median(np.sin(elevation))
-        xhat = np.sin(azimuth) * np.cos(elevation)
-        yhat = np.cos(azimuth) * np.cos(elevation)
-        zhat = np.sin(elevation)
-
-        dims = snr.shape
-
-        # mean_snr = np.nanmean(snr, axis=1)
-        u_wind = np.full(dims[1], np.nan)
-        v_wind = np.full(dims[1], np.nan)
-        w_wind = np.full(dims[1], np.nan)
-        u_err = np.full(dims[1], np.nan)
-        v_err = np.full(dims[1], np.nan)
-        w_err = np.full(dims[1], np.nan)
-        residual = np.full(dims[1], np.nan)
-        chisq = np.full(dims[1], np.nan)
-        corr = np.full(dims[1], np.nan)
-
-        # Loop over each level
-        for ii in range(dims[1]):
-            ur1 = doppler[:, ii]
-            snr1 = snr[:, ii]
-            with warnings.catch_warnings():
-                warnings.filterwarnings('ignore', category=RuntimeWarning)
-                index = np.where((snr1 >= snr_threshold) & np.isfinite(ur1))[0]
-            count = index.size
-            if count >= 4:
-                ur1 = ur1[index]
-                xhat1 = xhat[index]
-                yhat1 = yhat[index]
-                zhat1 = zhat[index]
-
-                a = np.full((3, 3), np.nan)
-                b = np.full(3, np.nan)
-
-                a[0, 0] = np.sum(xhat1**2)
-                a[1, 0] = np.sum(xhat1 * yhat1)
-                a[2, 0] = np.sum(xhat1 * zhat1)
-
-                a[0, 1] = a[1, 0]
-                a[1, 1] = np.sum(yhat1**2)
-                a[2, 1] = np.sum(yhat1 * zhat1)
-
-                a[0, 2] = a[2, 0]
-                a[1, 2] = a[2, 1]
-                a[2, 2] = np.sum(zhat1**2)
-
-                b[0] = np.sum(ur1 * xhat1)
-                b[1] = np.sum(ur1 * yhat1)
-                b[2] = np.sum(ur1 * zhat1)
- 
-                ainv = np.linalg.inv(a)
-                condition = np.linalg.norm(a) * np.linalg.norm(ainv)  # Condition Number ?
-                if condition < condition_limit:
-                    c = b @ ainv
-                    u_wind[ii] = c[0]
-                    v_wind[ii] = c[1]
-                    w_wind[ii] = c[2]
-                    ur_fit = xhat1 * u_wind[ii] + yhat1 * v_wind[ii] + zhat1 * w_wind[ii]
-                    chisq[ii] = np.sum((ur_fit - ur1) ** 2)
-                    residual[ii] = np.sqrt(chisq[ii] / count)
-                    with warnings.catch_warnings():
-                        warnings.filterwarnings('ignore', category=RuntimeWarning)
-                        corr[ii] = np.corrcoef(ur_fit, ur1)[0, 1]
-                    u_err[ii] = np.sqrt((chisq[ii] / (count - 3)) * ainv[0, 0])
-                    v_err[ii] = np.sqrt((chisq[ii] / (count - 3)) * ainv[1, 1])
-                    w_err[ii] = np.sqrt((chisq[ii] / (count - 3)) * ainv[2, 2])
-
-        # Compute windspeed and direction
-        with warnings.catch_warnings():
-            warnings.filterwarnings('ignore', category=RuntimeWarning)
-            wspd = np.sqrt(u_wind**2 + v_wind**2)
-            wdir = np.degrees(np.arctan2(u_wind, v_wind) + np.pi)
-
-            wspd_err = np.sqrt((u_wind * u_err) ** 2 + (v_wind * v_err) ** 2) / wspd
-            wdir_err = np.degrees(
-                np.sqrt((u_wind * v_err) ** 2 + (v_wind * u_err) ** 2) / wspd**2
+        task.append(
+            dask.delayed(process_ppi_winds)(
+                time[scan_index], elevation[scan_index], azimuth[scan_index], snr[scan_index, :],
+                doppler[scan_index, :], rng, condition_limit, snr_threshold, remove_all_missing,
+                height_units
             )
-
-        if remove_all_missing and np.isnan(wspd).all():
-            continue
-
-        time = time[0] + (time[-1] - time[0]) / 2
-        time = time.reshape(
-            1,
         )
-        wspd = wspd.reshape(1, rng.size)
-        wdir = wdir.reshape(1, rng.size)
-        wspd_err = wspd_err.reshape(1, rng.size)
-        wdir_err = wdir_err.reshape(1, rng.size)
-        corr = corr.reshape(1, rng.size)
-        residual = residual.reshape(1, rng.size)
-        with warnings.catch_warnings():
-            warnings.filterwarnings('ignore', category=RuntimeWarning)
-            snr_mean = np.nanmean(snr, axis=0)
-        snr_mean = snr_mean.reshape(1, rng.size)
-        new_object = xr.Dataset(
-            {
-                'wind_speed': (
-                    ('time', 'height'),
-                    wspd,
-                    {'long_name': 'Wind speed', 'units': 'm/s'},
-                ),
-                'wind_direction': (
-                    ('time', 'height'),
-                    wdir,
-                    {'long_name': 'Wind direction', 'units': 'degree'},
-                ),
-                'wind_speed_error': (
-                    ('time', 'height'),
-                    wspd_err,
-                    {'long_name': 'Wind direction error', 'units': 'm/s'},
-                ),
-                'wind_direction_error': (
-                    ('time', 'height'),
-                    wdir_err,
-                    {'long_name': 'Wind direction error', 'units': 'degree'},
-                ),
-                'signal_to_noise_ratio': (
-                    ('time', 'height'),
-                    snr_mean,
-                    {
-                        'long_name': 'Signal to noise ratio mean over PPI scan',
-                        'units': '1',
-                    },
-                ),
-                'residual': (
-                    ('time', 'height'),
-                    residual,
-                    {
-                        'long_name': 'Residual values (Square Root of Chi Square)',
-                        'units': 'm/s',
-                    },
-                ),
-                'correlation': (
-                    ('time', 'height'),
-                    corr,
-                    {'long_name': 'Correlation coefficient', 'units': '1'},
-                ),
-            },
-            {
-                'time': ('time', time, {'long_name': 'Time in UTC'}),
-                'height': (
-                    'height',
-                    height,
-                    {'long_name': 'Height to center of bin', 'units': height_units},
-                ),
-            },
-        )
+    results = dask.compute(*task)
+    new_object = xr.concat(results, 'time')
 
-        if isinstance(return_obj, xr.core.dataset.Dataset):
-            return_obj = xr.concat([return_obj, new_object], dim='time')
-        else:
-            return_obj = new_object
+    if isinstance(return_obj, xr.core.dataset.Dataset):
+        return_obj = xr.concat([return_obj, new_object], dim='time')
+    else:
+        return_obj = new_object
 
     return return_obj
+
+
+def process_ppi_winds(time, elevation, azimuth, snr, doppler, rng, condition_limit,
+                      snr_threshold, remove_all_missing, height_units):
+    """
+    This function is for processing the winds using dask from the compute_winds_from_ppi
+    function.  This should not be used standalone.
+
+    """
+
+    height = rng * np.median(np.sin(elevation))
+    xhat = np.sin(azimuth) * np.cos(elevation)
+    yhat = np.cos(azimuth) * np.cos(elevation)
+    zhat = np.sin(elevation)
+
+    dims = np.shape(snr)
+
+    # mean_snr = np.nanmean(snr, axis=1)
+    u_wind = np.full(dims[1], np.nan)
+    v_wind = np.full(dims[1], np.nan)
+    w_wind = np.full(dims[1], np.nan)
+    u_err = np.full(dims[1], np.nan)
+    v_err = np.full(dims[1], np.nan)
+    w_err = np.full(dims[1], np.nan)
+    residual = np.full(dims[1], np.nan)
+    chisq = np.full(dims[1], np.nan)
+    corr = np.full(dims[1], np.nan)
+
+    # Loop over each level
+    for ii in range(dims[1]):
+        ur1 = doppler[:, ii]
+        snr1 = snr[:, ii]
+        with warnings.catch_warnings():
+            warnings.filterwarnings('ignore', category=RuntimeWarning)
+            index = np.where((snr1 >= snr_threshold) & np.isfinite(ur1))[0]
+        count = index.size
+        if count >= 4:
+            ur1 = ur1[index]
+            xhat1 = xhat[index]
+            yhat1 = yhat[index]
+            zhat1 = zhat[index]
+
+            a = np.full((3, 3), np.nan)
+            b = np.full(3, np.nan)
+
+            a[0, 0] = np.sum(xhat1**2)
+            a[1, 0] = np.sum(xhat1 * yhat1)
+            a[2, 0] = np.sum(xhat1 * zhat1)
+
+            a[0, 1] = a[1, 0]
+            a[1, 1] = np.sum(yhat1**2)
+            a[2, 1] = np.sum(yhat1 * zhat1)
+
+            a[0, 2] = a[2, 0]
+            a[1, 2] = a[2, 1]
+            a[2, 2] = np.sum(zhat1**2)
+
+            b[0] = np.sum(ur1 * xhat1)
+            b[1] = np.sum(ur1 * yhat1)
+            b[2] = np.sum(ur1 * zhat1)
+
+            ainv = np.linalg.inv(a)
+            condition = np.linalg.norm(a) * np.linalg.norm(ainv)  # Condition Number ?
+            if condition < condition_limit:
+                c = b @ ainv
+                u_wind[ii] = c[0]
+                v_wind[ii] = c[1]
+                w_wind[ii] = c[2]
+                ur_fit = xhat1 * u_wind[ii] + yhat1 * v_wind[ii] + zhat1 * w_wind[ii]
+                chisq[ii] = np.sum((ur_fit - ur1) ** 2)
+                residual[ii] = np.sqrt(chisq[ii] / count)
+                with warnings.catch_warnings():
+                    warnings.filterwarnings('ignore', category=RuntimeWarning)
+                    corr[ii] = np.corrcoef(ur_fit, ur1)[0, 1]
+                u_err[ii] = np.sqrt((chisq[ii] / (count - 3)) * ainv[0, 0])
+                v_err[ii] = np.sqrt((chisq[ii] / (count - 3)) * ainv[1, 1])
+                w_err[ii] = np.sqrt((chisq[ii] / (count - 3)) * ainv[2, 2])
+
+    # Compute windspeed and direction
+    with warnings.catch_warnings():
+        warnings.filterwarnings('ignore', category=RuntimeWarning)
+        wspd = np.sqrt(u_wind**2 + v_wind**2)
+        wdir = np.degrees(np.arctan2(u_wind, v_wind) + np.pi)
+
+        wspd_err = np.sqrt((u_wind * u_err) ** 2 + (v_wind * v_err) ** 2) / wspd
+        wdir_err = np.degrees(
+            np.sqrt((u_wind * v_err) ** 2 + (v_wind * u_err) ** 2) / wspd**2
+        )
+
+    if remove_all_missing and np.isnan(wspd).all():
+        return np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan
+
+    time = time[0] + (time[-1] - time[0]) / 2
+    time = time.reshape(
+        1,
+    )
+    wspd = wspd.reshape(1, rng.size)
+    wdir = wdir.reshape(1, rng.size)
+    wspd_err = wspd_err.reshape(1, rng.size)
+    wdir_err = wdir_err.reshape(1, rng.size)
+    corr = corr.reshape(1, rng.size)
+    residual = residual.reshape(1, rng.size)
+    with warnings.catch_warnings():
+        warnings.filterwarnings('ignore', category=RuntimeWarning)
+        snr_mean = np.nanmean(snr, axis=0)
+    snr_mean = snr_mean.reshape(1, rng.size)
+
+    new_object = xr.Dataset(
+        {
+            'wind_speed': (
+                ('time', 'height'),
+                wspd,
+                {'long_name': 'Wind speed', 'units': 'm/s'},
+            ),
+            'wind_direction': (
+                ('time', 'height'),
+                wdir,
+                {'long_name': 'Wind direction', 'units': 'degree'},
+            ),
+            'wind_speed_error': (
+                ('time', 'height'),
+                wspd_err,
+                {'long_name': 'Wind direction error', 'units': 'm/s'},
+            ),
+            'wind_direction_error': (
+                ('time', 'height'),
+                wdir_err,
+                {'long_name': 'Wind direction error', 'units': 'degree'},
+            ),
+            'signal_to_noise_ratio': (
+                ('time', 'height'),
+                snr_mean,
+                {
+                    'long_name': 'Signal to noise ratio mean over PPI scan',
+                    'units': '1',
+                },
+            ),
+            'residual': (
+                ('time', 'height'),
+                residual,
+                {
+                    'long_name': 'Residual values (Square Root of Chi Square)',
+                    'units': 'm/s',
+                },
+            ),
+            'correlation': (
+                ('time', 'height'),
+                corr,
+                {'long_name': 'Correlation coefficient', 'units': '1'},
+            ),
+        },
+        {
+            'time': ('time', time, {'long_name': 'Time in UTC'}),
+            'height': (
+                'height',
+                height,
+                {'long_name': 'Height to center of bin', 'units': height_units},
+            ),
+        },
+    )
+    return new_object
