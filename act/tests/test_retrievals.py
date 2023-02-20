6b67ca5be0a74008f3f17f8b78f2f3f657a31f59
Max Grover <mgroverwx@gmail.com>
2023-02-20 13:51:19 -0600

    Make sure files are sorted

--- act/tests/test_retrievals.py
+++ act/tests/test_retrievals.py
@@ -229,7 +229,7 @@ def test_calculate_pbl_liu_liang():
 
 
 def test_calculate_heffter_pbl():
-    files = glob.glob(act.tests.sample_files.EXAMPLE_TWP_SONDE_20060121)
+    files = sorted(glob.glob(act.tests.sample_files.EXAMPLE_TWP_SONDE_20060121))
     ds = act.io.armfiles.read_netcdf(files[0])
     ds['tdry'].attrs['units'] = 'degree_Celsius'
     ds = act.retrievals.sonde.calculate_pbl_heffter(ds)
