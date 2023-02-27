3cff444de5c9e6cf56a3fb9c3cc8fe2cc3e5a78a
AdamTheisen <atheisen@anl.gov>
2023-02-26 20:36:08 -0600

    ENH: Adding functionality to use time bounds to shift data as well as an example workflow for creating merged products.

---
+++ examples/workflows/plot_merged_product.py
@@ -0,0 +1,80 @@
+"""
+Merge multiple datasets
+-----------------------
+
+Example to merge multiple data products into one using ACT.
+Shows how to adjust the timestamp if the timestamps are at
+different part of the sample interval (left, right, center).
+Also shows how to apply QC information, merge and resample
+data using xarray and plot/write out the results.
+
+"""
+
+import act
+import xarray as xr
+import matplotlib.pyplot as plt
+
+# Set data files
+ebbr_file = act.tests.sample_files.EXAMPLE_EBBR3
+ecor_file = act.tests.sample_files.EXAMPLE_ECOR
+sebs_file = act.tests.sample_files.EXAMPLE_SEBS
+
+# Read data into datasets
+ds_ebbr = act.io.armfiles.read_netcdf(ebbr_file)
+ds_ecor = act.io.armfiles.read_netcdf(ecor_file)
+ds_sebs = act.io.armfiles.read_netcdf(sebs_file)
+
+# Check for ARM DQRs and add them to the QC variables
+ds_ebbr = act.qc.arm.add_dqr_to_qc(ds_ebbr)
+ds_ecor = act.qc.arm.add_dqr_to_qc(ds_ecor)
+ds_sebs = act.qc.arm.add_dqr_to_qc(ds_sebs)
+
+# The ECOR and EBBR have different definitions of latent heat
+# flux and what is positive vs negative.  Check out the ARM
+# Handbooks for more information
+ds_ecor['lv_e'].values = ds_ecor['lv_e'].values * -1.
+
+# For example purposes, let's rename the ecor latent heat flux
+ds_ecor = ds_ecor.rename({'lv_e': 'latent_heat_flux_ecor'})
+ds_ecor['latent_heat_flux_ecor'].attrs['ancillary_variables'] = 'qc_latent_heat_flux_ecor'
+ds_ecor = ds_ecor.rename({'qc_lv_e': 'qc_latent_heat_flux_ecor'})
+
+# Also going to Switch some QC for example purposes
+qc = ds_ecor['qc_latent_heat_flux_ecor'].values
+qc[10:20] = 2
+ds_ecor['qc_latent_heat_flux_ecor'].values = qc
+
+# There is a difference in how these timestamps are defined
+# The EBBR is at the end of the sampling interval and the
+# ECOR is at the beginning.  Knowing this, we can shift the
+# EBBR timestampes by 30 minutes to coincide with the ECOR
+ds_ebbr = act.utils.datetime_utils.adjust_timestamp(ds_ebbr, offset=-30*60)
+
+# Now, we can merge all these objects into one product
+ds = xr.merge([ds_ecor, ds_ebbr, ds_sebs], compat='override')
+
+# Apply the QC information to set all flagged data to missing/NaN
+ds.qcfilter.datafilter(del_qc_var=False, rm_assessments=['Bad', 'Incorrect', 'Indeterminate', 'Suspect'])
+
+# Plot up data from the merged object for each of the instruments
+display = act.plotting.TimeSeriesDisplay(ds, figsize=(15, 10), subplot_shape=(3,))
+display.plot('latent_heat_flux_ecor', label='ECOR', subplot_index=(0,))
+display.plot('latent_heat_flux', label='EBBR', subplot_index=(0,))
+plt.legend()
+display.plot('surface_soil_heat_flux_1', label='SEBS', subplot_index=(1,))
+
+# Plot out the QC information that was modified as well
+display.qc_flag_block_plot('latent_heat_flux_ecor', subplot_index=(2,))
+plt.show()
+
+# Resample the data to 1 hour mean
+ds = ds.resample(time='H').mean(keep_attrs=True)
+
+# Plot up data from the hourly merged object for ECOR and EBBR
+display = act.plotting.TimeSeriesDisplay(ds, figsize=(15, 10), subplot_shape=(1,))
+display.plot('latent_heat_flux_ecor', label='ECOR', subplot_index=(0,))
+display.plot('latent_heat_flux', label='EBBR', subplot_index=(0,))
+plt.show()
+
+# Write data out to netcdf
+ds.to_netcdf('./sgpecor_ebbr_sebs.nc')
