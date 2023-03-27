e3698a46803cc393fe2bde896761062811b28afc
AdamTheisen <AdamTheisen@users.noreply.github.com>
2023-03-27 20:48:28 +0000

    deploy: 9c8940a35417a293f81485d0053e00513c08b3af

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.post1+g77aa557',
+    VERSION: '0.post1+g9c8940a',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
