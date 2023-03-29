bad3d94f81ff984a016ab6fddae2f1434fa33de6
zssherman <shermanx1991@gmail.com>
2023-03-29 13:28:29 -0500

    ADD: Adding simple hist_kwargs example.

---
+++ examples/plotting/plot_hist_kwargs.py
@@ -0,0 +1,23 @@
+"""
+Plot a histogram of Met data.
+----------------------------------------------------
+
+This is a simple example for how to plot a histogram
+of Meteorological data, while using hist_kwargs parameter.
+
+Author: Zachary Sherman
+"""
+
+from matplotlib import pyplot as plt
+
+import act
+
+files = act.tests.sample_files.EXAMPLE_MET1
+met_ds = act.io.armfiles.read_netcdf(files)
+
+# Plot data
+hist_kwargs = {'range': (-10, 10)}
+histdisplay = HistogramDisplay(ds)
+histdisplay.plot_stacked_bar_graph('temp_mean', bins=np.arange(-40, 40, 5),
+                                   hist_kwargs=hist_kwargs)
+plt.show()
