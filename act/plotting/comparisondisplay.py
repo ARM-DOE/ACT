025bf9000135eb50341109a54819e7efd82c11ff
jrobrien91 <obrienj@anl.gov>
2023-04-02 22:56:06 -0500

    ADD: Initial commit. Display object to compare two datastreams

---
+++ act/plotting/comparisondisplay.py
@@ -0,0 +1,196 @@
+""" Module for Plotting the Comparison of Datastreams and Variables. """
+
+import matplotlib.pyplot as plt
+import numpy as np
+import xarray as xr
+
+from ..utils import datetime_utils as dt_utils
+from .plot import Display
+
+
+class ComparisonDisplay(Display):
+    """
+    This class is used to make plots for comparing datastreams
+    and variables. It is inherited from Display and therefore 
+    contains all of Display's attributes and methods.
+
+    Examples
+    --------
+    To create a ComparisonDisplay with 3 rows, simply do:
+
+    .. code-block:: python
+
+        ds = act.read_netcdf(the_file)
+        disp = act.plotting.ComparisonDisplay(ds, subplot_shape=(3,), figsize=(15, 5))
+
+    The ComparisonDisplay constructor takes in the same keyword arguments as
+    plt.subplots. For more information on the plt.subplots keyword arguments,
+    see the `matplotlib documentation
+    <https://matplotlib.org/api/_as_gen/matplotlib.pyplot.subplots.html>`_.
+    If no subplot_shape is provided, then no figure or axis will be created
+    until add_subplots or plots is called.
+
+    """
+
+    def __init__(self, ds, subplot_shape=(1,), ds_name=None, **kwargs):
+        super().__init__(ds, subplot_shape, ds_name, **kwargs)
+
+    def set_xrng(self, xrng, subplot_index=(0,)):
+        """
+        Sets the x range of the plot.
+
+        Parameters
+        ----------
+        xrng : 2 number array
+            The x limits of the plot.
+        subplot_index : 1 or 2D tuple, list, or array
+            The index of the subplot to set the x range of.
+
+        """
+        if self.axes is None:
+            raise RuntimeError('set_xrng requires the plot to be displayed.')
+
+        if not hasattr(self, 'xrng') and len(self.axes.shape) == 2:
+            self.xrng = np.zeros((self.axes.shape[0], self.axes.shape[1], 2), dtype='datetime64[D]')
+        elif not hasattr(self, 'xrng') and len(self.axes.shape) == 1:
+            self.xrng = np.zeros((self.axes.shape[0], 2), dtype='datetime64[D]')
+
+        self.axes[subplot_index].set_xlim(xrng)
+        self.xrng[subplot_index, :] = np.array(xrng)
+
+    def set_yrng(self, yrng, subplot_index=(0,)):
+        """
+        Sets the y range of the plot.
+
+        Parameters
+        ----------
+        yrng : 2 number array
+            The y limits of the plot.
+        subplot_index : 1 or 2D tuple, list, or array
+            The index of the subplot to set the x range of.
+
+        """
+        if self.axes is None:
+            raise RuntimeError('set_yrng requires the plot to be displayed.')
+
+        if not hasattr(self, 'yrng') and len(self.axes.shape) == 2:
+            self.yrng = np.zeros((self.axes.shape[0], self.axes.shape[1], 2))
+        elif not hasattr(self, 'yrng') and len(self.axes.shape) == 1:
+            self.yrng = np.zeros((self.axes.shape[0], 2))
+
+        if yrng[0] == yrng[1]:
+            yrng[1] = yrng[1] + 1
+
+        self.axes[subplot_index].set_ylim(yrng)
+        self.yrng[subplot_index, :] = yrng
+
+    def _get_data(self, dsname, fields):
+        if isinstance(fields, str):
+            fields = [fields]
+        return self._ds[dsname][fields].dropna('time')
+
+    def scatter(
+        self,
+        x_field,
+        y_field,
+        m_field=None,
+        marker=None,
+        dsname=None,
+        subplot_index=(0,),
+        set_title=None,
+        set_shading='auto',
+        **kwargs,
+    ):
+        """
+        This procedure will produce a scatter plot from 2 variables.
+
+        Parameters
+        ----------
+        x_field : str
+            The name of the field to display on the X axis.
+        y_field : str
+            The name of the field to display on the Y axis.
+        m_field : str
+            The name of the field to display on the markers.
+        marker : str
+            The style of marker to use following matplotlib.markers
+            Defaults are points
+        dsname : str or None
+            The name of the datastream the field is contained in. Set
+            to None to let ACT automatically determine this.
+        subplot_index : tuple
+            The subplot index to place the plot in
+        set_title : str
+            The title of the plot.
+        set_shading : string
+            Option to to set the matplotlib.pcolormesh shading parameter.
+            Default to 'auto'
+
+        Other keyword arguments will be passed into :func:`matplotlib.pyplot.scatter`.
+
+        Returns
+        -------
+        ax : matplotlib axis handle
+            The matplotlib axis handle of the plot
+
+        """
+        if dsname is None and len(self._ds.keys()) > 1:
+            raise ValueError(
+                'You must choose a datastream when there are 2 '
+                'or more datasets in the TimeSeriesDisplay '
+                'object.'
+            )
+        elif dsname is None:
+            dsname = list(self._ds.keys())[0]
+
+        if m_field is None:
+            mdata = None
+            ds = self._get_data(dsname, [x_field, y_field])
+            xdata, ydata = ds[x_field], ds[y_field]
+        else:
+            ds = self._get_data(dsname, [x_field, y_field, m_field])
+            xdata, ydata, mdata = ds[x_field], ds[y_field], ds[m_field]
+
+        if 'units' in xdata.attrs:
+            xtitle = ''.join(['(', xdata.attrs['units'], ')'])
+        else:
+            xtitle = x_field
+
+        # Get the current plotting axis, add day/night background and plot data
+        if self.fig is None:
+            self.fig = plt.figure()
+
+        if self.axes is None:
+            self.axes = np.array([plt.axes()])
+            self.fig.add_axes(self.axes[0])
+
+        if 'units' in ydata.attrs:
+            ytitle = ''.join(['(', ydata.attrs['units'], ')'])
+        else:
+            ytitle = y_field
+
+        if marker is None:
+            marker = '.'
+
+        #xi, yi = np.meshgrid(x_inds, y_inds, indexing='ij')
+        #mesh = self.axes[subplot_index].pcolormesh(xi, yi, my_hist, shading=set_shading, **kwargs)
+        self.axes[subplot_index].scatter(xdata, ydata, c=mdata, marker=marker)
+
+        # Set Title
+        if set_title is None:
+            set_title = ' '.join(
+                [
+                    dsname,
+                    'on',
+                    dt_utils.numpy_to_arm_date(self._ds[dsname].time.values[0]),
+                ]
+            )
+        print(set_title)
+        self.axes[subplot_index].set_title(set_title)
+        self.axes[subplot_index].set_ylabel(ytitle)
+        self.axes[subplot_index].set_xlabel(xtitle)
+        #self.add_colorbar(mesh, title='count', subplot_index=subplot_index)
+
+        print('hey made it')
+
+        return self.axes[subplot_index]
