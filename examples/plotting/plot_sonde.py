71f6eb2e96ec9c44dd1b0f4b52f0e0fa27675a29
zssherman <shermanx1991@gmail.com>
2023-04-04 14:03:37 -0500

    MNT: Update example to show sonde data and not met data.

--- examples/plotting/plot_sonde.py
+++ examples/plotting/plot_sonde.py
@@ -12,20 +12,15 @@ from matplotlib import pyplot as plt
 
 import act
 
-files = act.tests.sample_files.EXAMPLE_MET_WILDCARD
-met_ds = act.io.armfiles.read_netcdf(files)
-print(met_ds)
-met_temp = met_ds.temp_mean
-met_rh = met_ds.rh_mean
-met_lcl = (20.0 + met_temp / 5.0) * (100.0 - met_rh) / 1000.0
-met_ds['met_lcl'] = met_lcl * 1000.0
-met_ds['met_lcl'].attrs['units'] = 'm'
-met_ds['met_lcl'].attrs['long_name'] = 'LCL Calculated from SGP MET E13'
+files = act.tests.sample_files.EXAMPLE_SONDE1
+sonde_ds = act.io.armfiles.read_netcdf(files)
+print(sonde_ds)
 
 # Plot data
-display = act.plotting.TimeSeriesDisplay(met_ds)
+display = act.plotting.TimeSeriesDisplay(sonde_ds)
 display.add_subplots((3,), figsize=(15, 10))
-display.plot('wspd_vec_mean', subplot_index=(0,))
-display.plot('temp_mean', subplot_index=(1,))
-display.plot('rh_mean', subplot_index=(2,))
+display.plot('wspd', subplot_index=(0,))
+display.plot('tdry', subplot_index=(1,))
+display.plot('rh', subplot_index=(2,))
 plt.show()
+
