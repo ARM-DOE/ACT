fix CAPE/CIN calculation

act/retrievals/sonde.py
@@ -98,7 +98,6 @@ def calculate_stability_indicies(
     temp_name='temperature',
     td_name='dewpoint_temperature',
     p_name='pressure',
-    rh_name='relative_humidity',
     moving_ave_window=0,
 ):
     """
@@ -115,8 +114,6 @@ def calculate_stability_indicies(
         The name of the dewpoint field.
     p_name : str
         The name of the pressure field.
-    rh_name : str
-        The name of the relative humidity field.
     moving_ave_window : int
         Number of points to do a moving average on sounding data to reduce
         noise. This is useful if noise in the sounding is preventing parcel
@@ -131,7 +128,6 @@ def calculate_stability_indicies(
     t = ds[temp_name]
     td = ds[td_name]
     p = ds[p_name]
-    rh = ds[rh_name]
 
     if not hasattr(t, 'units'):
         raise AttributeError('Temperature field must have units' + ' for ACT to discern!')
@@ -152,35 +148,24 @@ def calculate_stability_indicies(
         td_units = getattr(units, td.units)
 
     p_units = getattr(units, p.units)
-    rh_units = getattr(units, rh.units)
 
     # Sort all values by decreasing pressure
     t_sorted = np.array(t.values)
     td_sorted = np.array(td.values)
     p_sorted = np.array(p.values)
-    rh_sorted = np.array(rh.values)
     ind_sort = np.argsort(p_sorted)
     t_sorted = t_sorted[ind_sort[-1:0:-1]]
     td_sorted = td_sorted[ind_sort[-1:0:-1]]
     p_sorted = p_sorted[ind_sort[-1:0:-1]]
-    rh_sorted = rh_sorted[ind_sort[-1:0:-1]]
 
     if moving_ave_window > 0:
         t_sorted = np.convolve(t_sorted, np.ones((moving_ave_window,)) / moving_ave_window)
         td_sorted = np.convolve(td_sorted, np.ones((moving_ave_window,)) / moving_ave_window)
         p_sorted = np.convolve(p_sorted, np.ones((moving_ave_window,)) / moving_ave_window)
-        rh_sorted = np.convolve(rh_sorted, np.ones((moving_ave_window,)) / moving_ave_window)
 
     t_sorted = t_sorted * t_units
     td_sorted = td_sorted * td_units
     p_sorted = p_sorted * p_units
-    rh_sorted = rh_sorted * rh_units
-
-    # Calculate mixing ratio
-    mr = mpcalc.mixing_ratio_from_relative_humidity(p_sorted, t_sorted, rh_sorted)
-
-    # Discussion of issue #361 use virtual temperature.
-    vt = mpcalc.virtual_temperature(t_sorted, mr)
 
     t_profile = mpcalc.parcel_profile(p_sorted, t_sorted[0], td_sorted[0])
 
@@ -189,7 +174,9 @@ def calculate_stability_indicies(
     ds['parcel_temperature'].attrs['units'] = t_profile.units
 
     # Calculate CAPE, CIN, LCL
-    sbcape, sbcin = mpcalc.surface_based_cape_cin(p_sorted, vt, td_sorted)
+    sbcape, sbcin = mpcalc.surface_based_cape_cin(p_sorted,
+                                                  t_sorted,
+                                                  td_sorted)
 
     lcl = mpcalc.lcl(p_sorted[0], t_sorted[0], td_sorted[0])
     try:
@@ -197,7 +184,7 @@ def calculate_stability_indicies(
     except IndexError:
         lfc = np.nan * p_sorted.units
 
-    mucape, mucin = mpcalc.most_unstable_cape_cin(p_sorted, vt, td_sorted)
+    mucape, mucin = mpcalc.most_unstable_cape_cin(p_sorted, t_sorted, td_sorted)
 
     where_500 = np.argmin(np.abs(p_sorted - 500 * units.hPa))
     li = t_sorted[where_500] - t_profile[where_500]

act/tests/test_retrievals.py
@@ -24,7 +24,7 @@ def test_get_stability_indices():
         [269.85, 269.745276, 269.678006, 269.622444, 269.572331],
         rtol=1e-5)
     assert sonde_ds['parcel_temperature'].attrs['units'] == 'kelvin'
-    np.testing.assert_almost_equal(sonde_ds['surface_based_cape'], 0.62, decimal=2)
+    np.testing.assert_almost_equal(sonde_ds['surface_based_cape'], 1.6223, decimal=2)
     assert sonde_ds['surface_based_cape'].attrs['units'] == 'J/kg'
     assert sonde_ds['surface_based_cape'].attrs['long_name'] == 'Surface-based CAPE'
     np.testing.assert_almost_equal(sonde_ds['surface_based_cin'], 0.000, decimal=3)