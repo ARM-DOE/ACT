73f72809c53ee6b21efe1210ab1b65d61c5f6e17
zssherman <shermanx1991@gmail.com>
2023-05-10 16:37:22 -0500

    ADD: Adding new sodar example.

---
+++ examples/io/plot_sodar.py
@@ -0,0 +1,28 @@
+"""
+Read and plot Sodar files
+-------------------------
+
+This example shows how to read and display Sodar data from the Argonne
+National Laboratory (ANL) ATMOS site.
+
+Author: Zachary Sherman
+
+"""
+
+import matplotlib.pyplot as plt
+
+import act
+
+# Call the read_sodar function.
+# Example file is a Sodar at the ATMOS site.
+ds = act.io.read_sodar(act.tests.EXAMPLE_SODAR)
+
+# Create an ACT TimeSeriesDisplay.
+display = act.plotting.TimeSeriesDisplay({'Shear, Wind Direction, and Speed at ANL ATMOS': ds}, 
+                                         subplot_shape=(1,), figsize=(15, 5))
+
+# Plot shear with a wind barb overlay, while using a color vision
+# deficiency (CVD) colormap.
+display.plot('shear', subplot_index=(0,), cb_friendly=True)
+display.plot_barbs_from_spd_dir('speed', 'dir')
+plt.show()
