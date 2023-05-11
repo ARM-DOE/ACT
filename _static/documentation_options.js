71e9ddd5c49aad3f3d7540cba64f22039a7680db
mgrover1 <mgrover1@users.noreply.github.com>
2023-05-11 19:12:04 +0000

    deploy: feed9dcbe1189e361d5999db617ad9d6c90096bb

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.post1+g36063c6',
+    VERSION: '0.post1+gfeed9dc',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
