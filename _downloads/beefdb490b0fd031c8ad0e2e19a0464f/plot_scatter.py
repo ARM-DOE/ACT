b274c8c87d07795b161dd407e11ce5f72c26bb43
zssherman <zssherman@users.noreply.github.com>
2023-06-15 14:48:07 +0000

    deploy: dd0fdbb0b66439ea6c07a744dd5812a1ac4f69fe

---
+++ _downloads/beefdb490b0fd031c8ad0e2e19a0464f/plot_scatter.py
@@ -0,0 +1,36 @@
+"""
+Compare Aircraft Airspeeds
+--------------------------
+
+Compare Aircraft Airspeeds via the DistributionDisplay
+Scatter Plot
+
+Written: Joe O'Brien
+
+"""
+
+import act
+
+from act.io.icartt import read_icartt
+
+# Call the read_icartt function, which supports input
+# for ICARTT (v2.0) formatted files.
+# Example file is ARM Aerial Facility Navigation Data
+ds = read_icartt(act.tests.EXAMPLE_AAF_ICARTT)
+
+# Create a DistributionDisplay object to compare fields
+display = act.plotting.DistributionDisplay(ds)
+
+# Compare aircraft ground speed with indicated airspeed
+display.scatter('true_airspeed',
+                'ground_speed',
+                m_field='ambient_temp',
+                marker='x',
+                cbar_label='Ambient Temperature ($^\circ$C)'
+                )
+
+# Set the range of the field on the x-axis
+display.set_xrng((40, 140))
+display.set_yrng((40, 140))
+# Display the 1:1 ratio line
+display.set_ratio_line()
--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.post1+ga69a883',
+    VERSION: '0.post1+gdd0fdbb',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
