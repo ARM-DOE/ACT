80b154d51d9a2b1ed0d36165b85038cf6b0c64f2
AdamTheisen <atheisen@anl.gov>
2023-06-19 06:54:08 -0500

    ENH: Bug fix for discovery test

--- act/tests/test_discovery.py
+++ act/tests/test_discovery.py
@@ -291,6 +291,6 @@ def test_arm_doi():
 
 
 def test_download_surfrad():
-    results = act.discovery.download_surfrad_data(site='tbl', startdate='20230601', enddate='20230602')
+    results = act.discovery.download_surfrad(site='tbl', startdate='20230601', enddate='20230602')
     assert len(results) == 2
     assert 'tbl23152.dat' in results[0]
