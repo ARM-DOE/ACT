eaf6fee0105175d8ebad0bfc2fd20c2e4abf6a8d
zssherman <shermanx1991@gmail.com>
2023-09-26 14:43:49 -0500

    ADD: Adding templates for ACT examples.

---
+++ examples/templates/example_template.py
@@ -0,0 +1,18 @@
+# Place python module imports here, example:
+import act
+
+# Place arm username and token or example file if username and token
+# aren't set, example:
+username = os.getenv('ARM_USERNAME')
+token = os.getenv('ARM_PASSWORD')
+
+# Download and read file or files with the IO and discovery functions
+# within ACT, example:
+results = act.discovery.download_data(
+    username, token, 'sgpceilC1.b1', '2022-01-14', '2022-01-19')
+ceil_ds = act.io.armfiles.read_netcdf(results)
+
+# Plot file using the ACT display submodule, example:
+display = act.plotting.TimeSeriesDisplay(ceil_ds)
+display.plot('backscatter')
+plt.show()
