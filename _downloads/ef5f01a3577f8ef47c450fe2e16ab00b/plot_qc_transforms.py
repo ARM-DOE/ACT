5ab439774d8e8c6f91b8191d47728fb01516a933
AdamTheisen <AdamTheisen@users.noreply.github.com>
2023-10-13 16:44:31 +0000

    deploy: 9550ffe3043a68930296ca7a33895b1eb466b781

---
+++ _downloads/ef5f01a3577f8ef47c450fe2e16ab00b/plot_qc_transforms.py
@@ -0,0 +1,48 @@
+"""
+Transformations and QC
+----------------------
+
+Built-in transformations using xarray are not
+quality-control aware.  This example shows how
+a user should apply QC prior to performing transformations.
+
+"""
+
+import act
+import xarray as xr
+import matplotlib.pyplot as plt
+
+# Read in some sample MFRSR data and clean up the QC
+ds = act.io.armfiles.read_netcdf(act.tests.sample_files.EXAMPLE_MFRSR, cleanup_qc=True)
+
+# Let's resample the data to 5 minutes and take the mean
+ds_5min = ds.resample(time='5min').mean()
+
+variable = 'diffuse_hemisp_narrowband_filter4'
+
+# Let's look at a before and after of one of the qc variables
+print('With no QC applied before transformation')
+print('Before (10 1-minute samples): ', ds['qc_' + variable].values[0:10])
+print('After: (2 5-minute averages)', ds_5min['qc_' + variable].values[0:2])
+
+# That new QC variable does not make sense at all and should be an int
+# What needs to happen is that we apply QC as the user see's fit to all
+# variables before the transformations take place.
+print('\nAverage of ', variable, ' before and after applying QC')
+print('Note the change in the second value')
+print('Before (2 5 - minute averages): ', ds[variable].values[0:2])
+
+ds.qcfilter.datafilter(rm_assessments=['Bad', 'Indeterminate'])
+ds_5minb = ds.resample(time='5min').mean()
+
+# Print out the corresponding variable values
+print('After: (2 5 - minute averages)', ds_5minb[variable].values[0:2])
+
+## Plot up the variable and qc block plot
+display = act.plotting.TimeSeriesDisplay({'Original': ds, 'Average': ds_5min, 'Average_QCd': ds_5minb},
+                                         figsize=(15, 10), subplot_shape=(2,))
+display.plot(variable, dsname='Original', subplot_index=(0,), day_night_background=True)
+display.plot(variable, dsname='Average', subplot_index=(1,), day_night_background=True, label='No QC')
+display.plot(variable, dsname='Average_QCd', subplot_index=(1,), day_night_background=True, label='QC')
+plt.legend()
+plt.show()
--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.post1+g9ec9b2f',
+    VERSION: '0.post1+g9550ffe',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
