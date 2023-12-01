2774b530ddc322cebeac3ebb7cbdfcfbc69dbdfc
zssherman <shermanx1991@gmail.com>
2023-11-30 22:04:51 -0600

    FIX: Accidently removed part of a tests.

--- act/tests/test_retrievals.py
+++ act/tests/test_retrievals.py
@@ -258,6 +258,11 @@ def test_sp2_waveform_stats():
     np.testing.assert_almost_equal(np.nanmax(my_binary.PkHt_ch0.values), 98708.92915295, decimal=1)
     np.testing.assert_almost_equal(np.nanmax(my_binary.PkHt_ch4.values), 65088.39598033, decimal=1)
 
+
+@pytest.mark.skipif(not PYSP2_AVAILABLE, reason='PySP2 is not installed.')
+def test_sp2_psds():
+    my_sp2b = act.io.read_sp2(act.tests.EXAMPLE_SP2B)
+    my_ini = act.tests.EXAMPLE_INI
     my_binary = act.qc.get_waveform_statistics(my_sp2b, my_ini, parallel=False)
     my_hk = act.io.read_hk_file(act.tests.EXAMPLE_HK)
     my_binary = act.retrievals.calc_sp2_diams_masses(my_binary)
