cf5b9a580de7f54ec32714274c57713cfc1b55c0
AdamTheisen <atheisen@anl.gov>
2024-08-21 10:00:45 -0500

    ENH: Updating tests

--- tests/discovery/test_improve.py
+++ tests/discovery/test_improve.py
@@ -2,10 +2,12 @@ import numpy as np
 import act
 
 
-def test_get_airnow():
+def test_get_improve():
     ds = act.discovery.get_improve_data(site_id='244', start_date='1/1/2023', end_date='12/31/2023')
 
-    assert len(list(ds)) == 214
+    assert len(list(ds)) == 216
+    assert 'lat' in ds
+    assert 'lon' in ds
     assert len(ds.time.values) == 121
     assert 'aluminum_fine' in ds
     assert ds['ammonium_nitrate_fine'].values[0] == 1.41363
