4b2f0eb010f5d27d4b9c4f7b3ffed6574bbc377b
Ken Kehoe <kkehoe@ou.edu>
2024-08-28 16:07:19 -0600

    Catching warning with pytest to ensure the warning was issued

--- tests/plotting/test_windrosedisplay.py
+++ tests/plotting/test_windrosedisplay.py
@@ -138,15 +138,16 @@ def test_groupby_plot():
     # Create Plot Display
     display = WindRoseDisplay(ds, figsize=(15, 15), subplot_shape=(3, 3))
     groupby = display.group_by('day')
-    groupby.plot_group(
-        'plot_data',
-        None,
-        dir_field='wdir_vec_mean',
-        spd_field='wspd_vec_mean',
-        data_field='temp_mean',
-        num_dirs=12,
-        plot_type='line',
-    )
+    with pytest.warns(RuntimeWarning):
+        groupby.plot_group(
+            'plot_data',
+            None,
+            dir_field='wdir_vec_mean',
+            spd_field='wspd_vec_mean',
+            data_field='temp_mean',
+            num_dirs=12,
+            plot_type='line',
+        )
 
     # Set theta tick markers for each axis inside display to be inside the polar axes
     for i in range(3):
