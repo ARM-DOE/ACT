7c9586874800dcc463228722aa78c956937bcf23
zssherman <zssherman@users.noreply.github.com>
2024-10-15 19:12:16 +0000

    deploy: 70a757c1b67f23597879e116bf9c10e344f9d926

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.post1+g0ff55ec',
+    VERSION: '0.post1+g70a757c',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
