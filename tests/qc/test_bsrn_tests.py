4dac2c2043f30f516360969e2fd11757ddcd4b83
Ken Kehoe <kkehoe@ou.edu>
2024-10-29 15:39:03 -0600

    Adding tests for new retrievals and QC tests.

--- tests/qc/test_bsrn_tests.py
+++ tests/qc/test_bsrn_tests.py
@@ -232,6 +232,11 @@ def test_bsrn_limits_test():
         )
         ds['up_long_hemisp'].values[0:100] = ds['up_long_hemisp'].values[0:100] - 200
 
+        # Closure test modified data
+        ds['down_short_diffuse_hemisp'].values[714:814] = (
+            ds['down_short_diffuse_hemisp'].values[714:814] + 50.0
+        )
+
         ds.qcfilter.bsrn_comparison_tests(
             [
                 'Global over Sum SW Ratio',
@@ -240,6 +245,7 @@ def test_bsrn_limits_test():
                 'LW down to air temp',
                 'LW up to air temp',
                 'LW down to LW up',
+                'Closure',
             ],
             gbl_SW_dn_name='down_short_hemisp',
             glb_diffuse_SW_dn_name='down_short_diffuse_hemisp',
@@ -256,11 +262,11 @@ def test_bsrn_limits_test():
 
         # Ratio of Global over Sum SW
         result = ds.qcfilter.get_qc_test_mask('down_short_hemisp', test_number=5)
-        assert np.sum(result) == 190
+        assert np.sum(result) == 276
 
         # Diffuse Ratio
         result = ds.qcfilter.get_qc_test_mask('down_short_hemisp', test_number=6)
-        assert np.sum(result) == 47
+        assert np.sum(result) == 103
 
         # Shortwave up comparison
         result = ds.qcfilter.get_qc_test_mask('up_short_hemisp', test_number=5)
@@ -274,6 +280,26 @@ def test_bsrn_limits_test():
         result = ds.qcfilter.get_qc_test_mask('down_long_hemisp_shaded', test_number=5)
         assert np.sum(result) == 976
 
-        # Lonwave down to longwave up comparison
+        # Longwave down to longwave up comparison
         result = ds.qcfilter.get_qc_test_mask('down_long_hemisp_shaded', test_number=6)
         assert np.sum(result) == 100
+
+        # Closure test
+        assert (
+            ds['qc_down_short_hemisp'].attrs['flag_meanings'][6]
+            == 'Closure test indicating value outside of expected range'
+        )
+        result = ds.qcfilter.get_qc_test_mask('down_short_hemisp', test_number=7)
+        assert np.sum(result) == 38
+        assert (
+            ds['qc_short_direct_normal'].attrs['flag_meanings'][6]
+            == 'Closure test indicating value outside of expected range'
+        )
+        result = ds.qcfilter.get_qc_test_mask('short_direct_normal', test_number=7)
+        assert np.sum(result) == 38
+        assert (
+            ds['qc_down_short_diffuse_hemisp'].attrs['flag_meanings'][7]
+            == 'Closure test indicating value outside of expected range'
+        )
+        result = ds.qcfilter.get_qc_test_mask('down_short_diffuse_hemisp', test_number=8)
+        assert np.sum(result) == 38
--- tests/retrievals/test_radiation.py
+++ tests/retrievals/test_radiation.py
@@ -7,9 +7,65 @@ import act
 def test_calculate_sirs_variable():
     sirs_ds = act.io.arm.read_arm_netcdf(act.tests.sample_files.EXAMPLE_SIRS)
     met_ds = act.io.arm.read_arm_netcdf(act.tests.sample_files.EXAMPLE_MET1)
+    mfrsr_ds = act.io.arm.read_arm_netcdf(act.tests.sample_files.EXAMPLE_MFRSR)
 
+    elevation, _, _ = act.utils.geo_utils.get_solar_azimuth_elevation(
+        sirs_ds['lat'].values, sirs_ds['lon'].values, sirs_ds['time'].values
+    )
+    sirs_ds['solar_zenith_angle'] = xr.DataArray(
+        90.0 - elevation,
+        dims=['time'],
+        attrs={
+            'long_name': 'Solar zenith angle',
+            'units': 'degrees',
+        },
+    )
+
+    # old
     ds = act.retrievals.radiation.calculate_dsh_from_dsdh_sdn(sirs_ds)
     assert np.isclose(np.nansum(ds['derived_down_short_hemisp'].values), 61157.71, atol=0.1)
+
+    # new
+    ds = act.retrievals.radiation.calculate_ghi_from_dni_dhi(sirs_ds)
+    assert np.isclose(np.nansum(ds['derived_down_short_hemisp'].values), 61157.71, atol=0.1)
+
+    # Test with providing solar_zenith variable in Dataset
+    ds = act.retrievals.radiation.calculate_ghi_from_dni_dhi(
+        sirs_ds, solar_zenith='solar_zenith_angle'
+    )
+    assert np.isclose(np.nansum(ds['derived_down_short_hemisp'].values), 61157.71, atol=0.1)
+
+    ds = act.retrievals.radiation.calculate_dhi_from_dni_ghi(sirs_ds)
+    assert np.isclose(np.nansum(ds['derived_down_short_diffuse_hemisp'].values), 59825.39, atol=0.1)
+
+    # Test with providing solar_zenith variable in Dataset
+    ds = act.retrievals.radiation.calculate_dhi_from_dni_ghi(
+        sirs_ds, solar_zenith='solar_zenith_angle'
+    )
+    assert np.isclose(np.nansum(ds['derived_down_short_diffuse_hemisp'].values), 59825.39, atol=0.1)
+
+    # Test direct normal
+    mfrsr_ds = act.retrievals.radiation.calculate_dni_from_dhi_ghi(
+        mfrsr_ds,
+        ghi='hemisp_narrowband_filter3',
+        dhi='diffuse_hemisp_narrowband_filter3',
+        derived='derived_direct_normal_narrowband_filter3',
+    )
+    assert np.isclose(
+        np.nansum(mfrsr_ds['derived_direct_normal_narrowband_filter3'].values), 2553.92, atol=0.1
+    )
+
+    # Test with providing solar_zenith variable in Dataset
+    mfrsr_ds = act.retrievals.radiation.calculate_dni_from_dhi_ghi(
+        mfrsr_ds,
+        ghi='hemisp_narrowband_filter3',
+        dhi='diffuse_hemisp_narrowband_filter3',
+        derived='derived_direct_normal_narrowband_filter3',
+        solar_zenith='solar_zenith_angle',
+    )
+    assert np.isclose(
+        np.nansum(mfrsr_ds['derived_direct_normal_narrowband_filter3'].values), 2553.59, atol=0.1
+    )
 
     ds = act.retrievals.radiation.calculate_irradiance_stats(
         ds,
