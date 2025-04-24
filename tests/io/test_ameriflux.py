db221f60b68e99d7a92b46b391975931312fadac
zssherman <shermanx1991@gmail.com>
2025-04-24 09:48:52 -0500

    TST: Add test_ameriflux unit tests.

--- tests/io/test_ameriflux.py
+++ tests/io/test_ameriflux.py
@@ -1,7 +1,72 @@
 import act
 import glob
+import numpy as np
+from numpy.testing import assert_equal
 import xarray as xr
 import pytest
+
+
+def test_read_ameriflux():
+    rename_vars_dict = {'FETCH_70': 'foo'}
+
+    ds = act.io.read_ameriflux(
+        act.tests.EXAMPLE_AMERIFLUX_BASE,
+        metadata_filename=act.tests.EXAMPLE_AMERIFLUX_BIF,
+        date_type='BASE',
+        rename_vars_dict=rename_vars_dict,
+    )
+    # Test if variables are correct and units and fill values were added
+    assert 'H2O' in ds.variables.keys()
+    assert ds.variables['H2O'].attrs['units'] == 'mmolH2O mol-1'
+    assert ds.variables['H2O'].attrs['_FILL_VALUE'] == -9999
+    assert np.isnan(ds.variables['H2O'].values[0:5]).all()
+    assert_equal(ds.variables['H2O'].values[-5:], [6.40095, 6.31573, 6.04937, 6.04294, 5.70889])
+
+    assert 'T_SONIC' in ds.variables.keys()
+    assert ds.variables['T_SONIC'].attrs['units'] == 'deg C'
+    assert ds.variables['T_SONIC'].attrs['_FILL_VALUE'] == -9999
+    assert np.isnan(ds.variables['T_SONIC'].values[0:5]).all()
+    assert_equal(ds.variables['T_SONIC'].values[-5:], [4.30861, 4.19665, 4.16113, 3.99634, 3.86785])
+
+    # Test if time is correct
+    assert ds.time.values[0] == np.datetime64('2024-01-01T00:00:00.000000000')
+    assert len(ds.time.values) == 17568
+
+    # Test if renaming variables worked
+    assert 'foo' in ds.variables.keys()
+
+    # Test metadata
+    assert ds.attrs['site'] == 'US-CU1'
+    assert ds.attrs['version'] == '1-5'
+    assert ds.attrs['SITE_NAME'] == 'UIC Plant Research Laboratory Chicago'
+
+    assert len(ds.attrs['TEAM_MEMBERS']) == 9
+    assert 'Bhupendra Raut' in ds.attrs['TEAM_MEMBERS']
+    assert 'Matt Tuftedal' in ds.attrs['TEAM_MEMBERS']
+    assert 'TEAM_MEMBER_EMAIL' in ds.attrs['TEAM_MEMBERS']['Bhupendra Raut']
+    assert 'TEAM_MEMBER_INSTITUTION' in ds.attrs['TEAM_MEMBERS']['Bhupendra Raut']
+    assert (
+        ds.attrs['TEAM_MEMBERS']['Bhupendra Raut']['TEAM_MEMBER_INSTITUTION']
+        == 'Argonne National Laboratory'
+    )
+
+    assert 'CO2' in ds.attrs['FLUX_MEASUREMENTS_VARIABLE']
+    assert 'H' in ds.attrs['FLUX_MEASUREMENTS_VARIABLE']
+    assert (
+        ds.attrs['FLUX_MEASUREMENTS_VARIABLE']['H']['FLUX_MEASUREMENTS_DATE_START']
+        == '202407010000'
+    )
+    assert (
+        ds.attrs['FLUX_MEASUREMENTS_VARIABLE']['H']['FLUX_MEASUREMENTS_METHOD'] == 'Eddy Covariance'
+    )
+
+    assert 'Bhupendra Raut' in ds.attrs['DOI_CONTRIBUTOR_NAME']
+    assert 'Sujan Pal' in ds.attrs['DOI_CONTRIBUTOR_NAME']
+    assert (
+        ds.attrs['DOI_CONTRIBUTOR_NAME']['Bhupendra Raut']['DOI_CONTRIBUTOR_DATAPRODUCT']
+        == 'AmeriFlux'
+    )
+    assert ds.attrs['DOI_CONTRIBUTOR_NAME']['Bhupendra Raut']['DOI_CONTRIBUTOR_ROLE'] == 'Author'
 
 
 def test_convert_to_ameriflux():
