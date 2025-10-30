5188f441072509ab880f72d49ed0712473c41602
jrobrien91 <obrienj@anl.gov>
2025-10-30 12:36:08 -0500

    ENH: Adding pbl_lidar retrieval file

--- act/retrievals/__init__.py
+++ act/retrievals/__init__.py
@@ -7,12 +7,16 @@ import lazy_loader as lazy
 
 __getattr__, __dir__, __all__ = lazy.attach(
     __name__,
-    submodules=['aeri', 'cbh', 'doppler_lidar', 'irt', 'radiation', 'sonde', 'sp2'],
+    submodules=['aeri', 'cbh', 'doppler_lidar', 'irt', 'pbl_lidar', 'radiation', 'sonde', 'sp2'],
     submod_attrs={
         'aeri': ['aeri2irt'],
         'cbh': ['generic_sobel_cbh'],
         'doppler_lidar': ['compute_winds_from_ppi'],
         'irt': ['sst_from_irt', 'sum_function_irt'],
+        'pbl_lidar': [
+            'calculate_gradient_pbl',
+            'calculate_modified_gradient_pbl',
+        ],
         'radiation': [
             'calculate_dsh_from_dsdh_sdn',
             'calculate_irradiance_stats',
