bf14f934a282ff2cef59f463ab3a7508de7b4ed7
zssherman <shermanx1991@gmail.com>
2026-01-14 11:05:18 -0600

    FIX: More scipy stat fixes.

--- act/plotting/timeseriesdisplay.py
+++ act/plotting/timeseriesdisplay.py
@@ -2129,7 +2129,9 @@ class TimeSeriesDisplay(Display):
 
         start = int(mdates.date2num(xdata.values[0]))
         end = int(mdates.date2num(xdata.values[-1]))
-        delta = stats.mode(xdata.diff('time').values)[0] / np.timedelta64(1, 'D')
+        diff_vals = xdata.diff('time').values
+        unique_vals, counts = np.unique(diff_vals, return_counts=True)
+        delta = unique_vals[np.argmax(counts)] / np.timedelta64(1, 'D')
 
         # Calculate mean for reference period and subtract from the data
         if reference_period is not None:
